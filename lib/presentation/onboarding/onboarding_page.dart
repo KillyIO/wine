1669dd1df3503306ebe15b5af8f53bfd52656b36
François Grèzes <grezesfrancois@gmail.com>
2021-03-15 23:04:58 +0100

    add onboarding page

---
+++ lib/presentation/onboarding/onboarding_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:wine/utils/themes.dart';
+
+/// @nodoc
+class OnboardingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(),
+      ),
+      value: lightTheme,
+    );
+  }
+}
