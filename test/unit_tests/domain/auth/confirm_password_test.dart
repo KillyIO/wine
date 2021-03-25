01229845a66970eb68c783a987ade8e5118efac4
François Grèzes <grezesfrancois@gmail.com>
2021-03-25 22:16:55 +0100

    update confirm password test wording

--- test/unit_tests/domain/auth/confirm_password_test.dart
+++ test/unit_tests/domain/auth/confirm_password_test.dart
@@ -5,43 +5,33 @@ import 'package:wine/domain/core/value_failure.dart';
 
 void main() {
   group('ConfirmPassword -', () {
-    test(
-      'When input And/Or input2 null Then throw AssertionError',
-      () {
-        expect(
-          () => ConfirmPassword(null, null),
-          throwsAssertionError,
-        );
-      },
-    );
+    test('should return AssertionError if inputs null', () {
+      expect(
+        () => ConfirmPassword(null, null),
+        throwsAssertionError,
+      );
+    });
+
+    test('should return input if inputs valid and equal', () {
+      final password = ConfirmPassword(':TA..2Qjp{+kRp#R', ':TA..2Qjp{+kRp#R');
+
+      expect(
+        password.value,
+        right(':TA..2Qjp{+kRp#R'),
+      );
+    });
+
+    test('should return ValueFailure.invalidPassword if one input invalid', () {
+      final password = ConfirmPassword('myname123', ':TA..2Qjp{+kRp#R');
+
+      expect(
+        password.value,
+        left(const ValueFailure<String>.invalidPassword('myname123')),
+      );
+    });
 
     test(
-      'When input And input2 valid And input equal input2 Then return input',
-      () {
-        final password =
-            ConfirmPassword(':TA..2Qjp{+kRp#R', ':TA..2Qjp{+kRp#R');
-
-        expect(
-          password.value,
-          right(':TA..2Qjp{+kRp#R'),
-        );
-      },
-    );
-
-    test(
-      'When input invalid Then return ValueFailure invalidPassword',
-      () {
-        final password = ConfirmPassword('myname123', ':TA..2Qjp{+kRp#R');
-
-        expect(
-          password.value,
-          left(const ValueFailure<String>.invalidPassword('myname123')),
-        );
-      },
-    );
-
-    test(
-      '''When input valid BUT not equal input2 Then return ValueFailure invalidConfirmPassword''',
+      'should return ValueFailure.invalidConfirmPassword if inputs not equal',
       () {
         final password =
             ConfirmPassword(':TA..2Qjp{+kRp#R', ':TB..2Qjp{+kRp#R');
