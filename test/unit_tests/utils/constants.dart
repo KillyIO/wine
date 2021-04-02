e0e034ef64bdcb11cdce264ef2244a5d02985798
François Grèzes <grezesfrancois@gmail.com>
2021-04-03 00:31:27 +0200

    add name to test constants

--- test/unit_tests/utils/constants.dart
+++ test/unit_tests/utils/constants.dart
@@ -10,6 +10,7 @@ const testEmailAddress = 'lphong.tieu.75@pickuplanet.com';
 const testPassword = '''wT-t"_fCznEH+tPMt7Y\$JB''';
 const testUid = '73488ab6-8db0-11eb-8dcd-0242ac130003';
 const testUsername = 'findingnemo';
+const testName = 'Finding Nemo';
 
 const testSettings = Settings(
   enableChaptersBookmarksCount: false,
