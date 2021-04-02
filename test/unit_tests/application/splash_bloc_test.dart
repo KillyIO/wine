658fdfc7e15e4a69cd7783a261cb62960187be04
François Grèzes <grezesfrancois@gmail.com>
2021-04-02 19:25:15 +0200

    add splash bloc tests

--- test/unit_tests/application/splash_bloc_test.dart
+++ test/unit_tests/application/splash_bloc_test.dart
@@ -3,13 +3,17 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:wine/application/splash/splash_bloc.dart';
+import 'package:wine/domain/auth/auth_failure.dart';
 import 'package:wine/domain/auth/i_auth_facade.dart';
+import 'package:wine/domain/core/core_failure.dart';
+import 'package:wine/domain/default_covers/default_covers_failure.dart';
 import 'package:wine/domain/default_covers/i_default_covers_repository.dart';
 import 'package:wine/domain/sessions/i_sessions_repository.dart';
 import 'package:wine/domain/sessions/sessions_failure.dart';
 import 'package:wine/domain/settings/i_ssettings_repository.dart';
 import 'package:wine/domain/settings/settings_failure.dart';
 import 'package:wine/domain/user/i_user_repository.dart';
+import 'package:wine/domain/user/user_failure.dart';
 
 import '../../mocks/auth_mocks.dart';
 import '../../mocks/default_covers_mock.dart';
@@ -54,7 +58,155 @@ void main() {
       expect: <SplashState>[],
     );
 
-    group('errors -', () {});
+    group('errors -', () {
+      blocTest(
+        'When error occur during signInAnonymously then yield Failure',
+        build: () => _splashBloc,
+        act: (bloc) {
+          when(() => _authFacade.isLoggedIn).thenReturn(false);
+          when(() => _authFacade.logInAnonymously())
+              .thenAnswer((_) async => left(const AuthFailure.serverError()));
+          return bloc.add(const SplashEvent.splashPageLaunched());
+        },
+        expect: <SplashState>[
+          const SplashState.failure(
+            CoreFailure.auth(AuthFailure.serverError()),
+          ),
+        ],
+        verify: (_) {
+          verifyInOrder([
+            () => _authFacade.isLoggedIn,
+            _authFacade.logInAnonymously,
+          ]);
+        },
+      );
+
+      blocTest(
+        'When error occur during loadDefaultCoverURLs then yield Failure',
+        build: () => _splashBloc,
+        act: (bloc) {
+          when(() => _authFacade.isAnonymous).thenReturn(false);
+          when(() => _defaultCoversRepository.loadDefaultCoverURLs())
+              .thenAnswer((_) async =>
+                  left(const DefaultCoversFailure.defaultCoverURLsNotLoaded()));
+          return bloc.add(const SplashEvent.authenticated());
+        },
+        expect: <SplashState>[
+          const SplashState.failure(
+            CoreFailure.defaultCovers(
+              DefaultCoversFailure.defaultCoverURLsNotLoaded(),
+            ),
+          ),
+        ],
+        verify: (_) {
+          verifyInOrder([
+            () => _authFacade.isAnonymous,
+            _defaultCoversRepository.loadDefaultCoverURLs,
+          ]);
+        },
+      );
+
+      blocTest(
+        'When error occur during cacheDefaultCoverURLs then yield Failure',
+        build: () => _splashBloc,
+        act: (bloc) {
+          when(() => _defaultCoversRepository.cacheDefaultCoverURLs({}))
+              .thenAnswer((_) async =>
+                  left(const DefaultCoversFailure.defaultCoverURLsNotCached()));
+          return bloc.add(const SplashEvent.defaultCoverURLsLoaded({}));
+        },
+        expect: <SplashState>[
+          const SplashState.failure(
+            CoreFailure.defaultCovers(
+              DefaultCoversFailure.defaultCoverURLsNotCached(),
+            ),
+          ),
+        ],
+        verify: (_) {
+          verify(() => _defaultCoversRepository.cacheDefaultCoverURLs({}))
+              .called(1);
+        },
+      );
+
+      blocTest(
+        'When error occur during initializeSettings then yield Failure',
+        build: () => _splashBloc,
+        act: (bloc) {
+          when(() => _settingsRepository.initializeSettings()).thenAnswer(
+            (_) async => left(const SettingsFailure.settingsNotInitialized()),
+          );
+          return bloc.add(const SplashEvent.settingsNotFound());
+        },
+        expect: <SplashState>[
+          const SplashState.failure(
+            CoreFailure.settings(SettingsFailure.settingsNotInitialized()),
+          ),
+        ],
+        verify: (_) {
+          verify(() => _settingsRepository.initializeSettings()).called(1);
+        },
+      );
+
+      blocTest(
+        'When error occur during createSession then yield Failure',
+        build: () => _splashBloc,
+        act: (bloc) {
+          when(() => _sessionsRepository.createSession()).thenAnswer(
+            (_) async => left(const SessionsFailure.sessionNotCreated()),
+          );
+          return bloc.add(const SplashEvent.sessionNotFound());
+        },
+        expect: <SplashState>[
+          const SplashState.failure(
+            CoreFailure.sessions(SessionsFailure.sessionNotCreated()),
+          ),
+        ],
+        verify: (_) {
+          verify(() => _sessionsRepository.createSession()).called(1);
+        },
+      );
+
+      blocTest(
+        'When error occur during loadUser then yield Failure',
+        build: () => _splashBloc,
+        act: (bloc) {
+          when(() => _authFacade.isAnonymous).thenReturn(false);
+          when(() => _userRepository.loadUser(testUid))
+              .thenAnswer((_) async => left(const UserFailure.userNotFound()));
+          return bloc.add(SplashEvent.sessionFetched(testUser));
+        },
+        expect: <SplashState>[
+          const SplashState.failure(
+            CoreFailure.user(UserFailure.userNotFound()),
+          ),
+        ],
+        verify: (_) {
+          verifyInOrder([
+            () => _authFacade.isAnonymous,
+            () => _userRepository.loadUser(testUid),
+          ]);
+        },
+      );
+
+      blocTest(
+        'When error occur during updateSession then yield Failure',
+        build: () => _splashBloc,
+        act: (bloc) {
+          when(() => _sessionsRepository.updateSession(testUser)).thenAnswer(
+            (_) async => left(const SessionsFailure.sessionNotUpdated()),
+          );
+          return bloc.add(SplashEvent.userLoaded(testUser));
+        },
+        expect: <SplashState>[
+          const SplashState.failure(
+            CoreFailure.sessions(SessionsFailure.sessionNotUpdated()),
+          ),
+        ],
+        verify: (_) {
+          verify(() => _sessionsRepository.updateSession(testUser)).called(1);
+        },
+      );
+    });
 
     group('completed -', () {
       blocTest(
@@ -152,60 +304,5 @@ void main() {
         },
       );
     });
-
-    // group('isLoggedin - true -', () {
-    //   setUp(() {
-    //     when(() => _authFacade.isLoggedIn).thenReturn(true);
-    //   });
-
-    //   blocTest(
-    //     'When error occur during loadDefaultCovers then yield Failure',
-    //     build: () => _splashBloc,
-    //     act: (bloc) {
-    //       when(() => _defaultCoversRepository.loadDefaultCoverURLs())
-    //           .thenAnswer((_) async =>
-    //               left(const DefaultCoversFailure.defaultCoverURLsNotLoaded()));
-    //       return bloc.add(const SplashEvent.splashPageLaunched());
-    //     },
-    //     expect: <SplashState>[
-    //       const SplashState.failure(CoreFailure.defaultCovers(
-    //         DefaultCoversFailure.defaultCoverURLsNotLoaded(),
-    //       )),
-    //     ],
-    //     verify: (_) {
-    //       verifyInOrder([
-    //         () => _authFacade.isLoggedIn,
-    //         _defaultCoversRepository.loadDefaultCoverURLs,
-    //       ]);
-    //     },
-    //   );
-    // });
-
-    // group('isLoggedin - false -', () {
-    //   setUp(() {
-    //     when(() => _authFacade.isLoggedIn).thenReturn(false);
-    //   });
-
-    //   blocTest(
-    //     'When error occur during signInAnonymously then yield Failure',
-    //     build: () => _splashBloc,
-    //     act: (bloc) {
-    //       when(() => _authFacade.logInAnonymously())
-    //           .thenAnswer((_) async => left(const AuthFailure.serverError()));
-    //       return bloc.add(const SplashEvent.splashPageLaunched());
-    //     },
-    //     expect: <SplashState>[
-    //       const SplashState.failure(
-    //         CoreFailure.auth(AuthFailure.serverError()),
-    //       ),
-    //     ],
-    //     verify: (_) {
-    //       verifyInOrder([
-    //         () => _authFacade.isLoggedIn,
-    //         _authFacade.logInAnonymously,
-    //       ]);
-    //     },
-    //   );
-    // });
   });
 }
