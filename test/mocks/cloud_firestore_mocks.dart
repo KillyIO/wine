3cec895e5f82cc5ba72e8aba9db62cbfe1bc488e
François Grèzes <grezesfrancois@gmail.com>
2021-04-06 22:13:18 +0200

    add some mocks + add default covers tests

--- test/mocks/cloud_firestore_mocks.dart
+++ test/mocks/cloud_firestore_mocks.dart
@@ -2,3 +2,13 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:mocktail/mocktail.dart';
 
 class MockFirebaseFirestore extends Mock implements FirebaseFirestore {}
+
+class MockCollectionReference extends Mock implements CollectionReference {}
+
+class MockDocumentReference extends Mock implements DocumentReference {}
+
+class MockDocumentSnapshot extends Mock implements DocumentSnapshot {}
+
+class MockQueryDocumentSnapshot extends Mock implements QueryDocumentSnapshot {}
+
+class MockQuerySnapshot extends Mock implements QuerySnapshot {}
--- test/unit_tests/infrastructure/default_covers_repository_test.dart
+++ test/unit_tests/infrastructure/default_covers_repository_test.dart
@@ -23,15 +23,13 @@ void main() {
     _hive = MockHiveInterface();
 
     _defaultCoversRepository = DefaultCoversRepository(_firestore, _hive);
+
+    when(() => _hive.openBox(any())).thenAnswer((_) async => _boxString);
   });
 
   group('cacheDefaultCoverURLs -', () {
-    setUp(() {
-      when(() => _hive.openBox(any())).thenAnswer((_) async => _boxString);
-    });
-
-    test('When covers cached Then returrn Unit', () async {
-      when(() => _boxString.get(any())).thenReturn('cover');
+    test('When covers cached Then return Unit', () async {
+      when(() => _boxString.get(any())).thenReturn('coverURL');
 
       final result = await _defaultCoversRepository
           .cacheDefaultCoverURLs(testDefaultCovers);
@@ -41,11 +39,9 @@ void main() {
         (success) => expect(success, unit),
       );
 
-      verify(() => _boxString.put('dc1', testDefaultCovers['dc1'])).called(1);
-      verify(() => _boxString.put('dc2', testDefaultCovers['dc2'])).called(1);
+      verify(() => _boxString.put(any(), any())).called(2);
 
-      verify(() => _boxString.get('dc1')).called(1);
-      verify(() => _boxString.get('dc2')).called(1);
+      verify(() => _boxString.get(any())).called(2);
     });
 
     test(
@@ -67,7 +63,95 @@ void main() {
     );
   });
 
-  group('fetchDefaultCoverURLByKey -', () {});
+  group('fetchDefaultCoverURLByKey -', () {
+    test('When cover fetched Then return cover URL', () async {
+      when(() => _boxString.get(any())).thenReturn('coverURL');
 
-  group('loadDefaultCoverURLs -', () {});
+      final result =
+          await _defaultCoversRepository.fetchDefaultCoverURLByKey('string');
+
+      result.fold(
+        (_) {},
+        (success) => expect(success, 'coverURL'),
+      );
+    });
+
+    test(
+      'When cover not found Then return DefaultCoverURLsNotFetched',
+      () async {
+        when(() => _boxString.get(any())).thenReturn(null);
+
+        final result =
+            await _defaultCoversRepository.fetchDefaultCoverURLByKey('string');
+
+        result.fold(
+          (failure) => expect(
+              failure, const DefaultCoversFailure.defaultCoverURLsNotFetched()),
+          (_) {},
+        );
+      },
+    );
+  });
+
+  group('loadDefaultCoverURLs -', () {
+    MockCollectionReference _collectionReference;
+    MockQuerySnapshot _querySnapshot;
+    MockQueryDocumentSnapshot _queryDocumentSnapshot;
+
+    setUp(() {
+      _collectionReference = MockCollectionReference();
+      _querySnapshot = MockQuerySnapshot();
+      _queryDocumentSnapshot = MockQueryDocumentSnapshot();
+    });
+
+    test('When cover URLs loaded Then return Map', () async {
+      when(() => _firestore.collection(any())).thenReturn(_collectionReference);
+      when(_collectionReference.get).thenAnswer((_) async => _querySnapshot);
+      when(_queryDocumentSnapshot.data).thenReturn(testDefaultCovers);
+
+      final result = await _defaultCoversRepository.loadDefaultCoverURLs();
+
+      result.fold(
+        (_) {},
+        (success) => expect(success, testDefaultCovers),
+      );
+    });
+
+    test('When docs empty Then return DefaultCoverURLsNotLoaded', () async {
+      when(() => _firestore.collection(any())).thenReturn(_collectionReference);
+      when(_collectionReference.get).thenAnswer((_) async => _querySnapshot);
+      when(() => _querySnapshot.docs).thenReturn(<MockQueryDocumentSnapshot>[]);
+
+      final result = await _defaultCoversRepository.loadDefaultCoverURLs();
+
+      result.fold(
+        (failure) => expect(
+            failure, const DefaultCoversFailure.defaultCoverURLsNotLoaded()),
+        (_) {},
+      );
+    });
+
+    test('When server error occurs Then return ServerError', () async {
+      when(() => _firestore.collection(any())).thenReturn(_collectionReference);
+      when(_collectionReference.get).thenThrow(testRandomServerException);
+
+      final result = await _defaultCoversRepository.loadDefaultCoverURLs();
+
+      result.fold(
+        (failure) => expect(failure, const DefaultCoversFailure.serverError()),
+        (_) {},
+      );
+    });
+
+    test('When unexpected error occurs Then return Unexpected', () async {
+      when(() => _firestore.collection(any())).thenReturn(null);
+
+      final result = await _defaultCoversRepository.loadDefaultCoverURLs();
+
+      result.fold(
+        (failure) => expect(failure, const DefaultCoversFailure.unexpected()),
+        (_) {},
+      );
+    });
+  });
 }
