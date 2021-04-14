0ad077c56fe530504852c9d13e22314da7564e37
François Grèzes <grezesfrancois@gmail.com>
2021-04-14 23:10:55 +0200

    add cover url to series model

--- lib/domain/series/series.dart
+++ lib/domain/series/series.dart
@@ -1,4 +1,6 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:wine/domain/core/cover_url.dart';
+import 'package:wine/domain/core/genre.dart';
 import 'package:wine/domain/core/unique_id.dart';
 
 part 'series.freezed.dart';
@@ -8,14 +10,20 @@ part 'series.freezed.dart';
 abstract class Series with _$Series {
   /// @nodoc
   factory Series({
-    @required bool isDeleted,
+    @required UniqueID authorUID,
+    @required CoverURL coverURL,
+    @required Genre genre,
+    @required Genre genreOptional,
     @required bool isNSFW,
+    @required Language language,
+    @required Subtitle subtitle,
+    @required Summary summary,
+    @required Title title,
     @required UniqueID uid,
   }) = _Series;
 
   /// @nodoc
   factory Series.empty() => Series(
-        isDeleted: false,
         isNSFW: false,
         uid: UniqueID(),
       );
--- lib/domain/series/series.freezed.dart
+++ lib/domain/series/series.freezed.dart
@@ -15,12 +15,30 @@ class _$SeriesTearOff {
 
 // ignore: unused_element
   _Series call(
-      {@required bool isDeleted,
+      {@required UniqueID authorUID,
+      @required CoverURL coverURL,
+      @required String deletionReason,
+      @required Genre genre,
+      @required Genre genreOptional,
+      @required bool isDeleted,
       @required bool isNSFW,
+      @required Language language,
+      @required Subtitle subtitle,
+      @required Summary summary,
+      @required Title title,
       @required UniqueID uid}) {
     return _Series(
+      authorUID: authorUID,
+      coverURL: coverURL,
+      deletionReason: deletionReason,
+      genre: genre,
+      genreOptional: genreOptional,
       isDeleted: isDeleted,
       isNSFW: isNSFW,
+      language: language,
+      subtitle: subtitle,
+      summary: summary,
+      title: title,
       uid: uid,
     );
   }
@@ -32,8 +50,17 @@ const $Series = _$SeriesTearOff();
 
 /// @nodoc
 mixin _$Series {
+  UniqueID get authorUID;
+  CoverURL get coverURL;
+  String get deletionReason;
+  Genre get genre;
+  Genre get genreOptional;
   bool get isDeleted;
   bool get isNSFW;
+  Language get language;
+  Subtitle get subtitle;
+  Summary get summary;
+  Title get title;
   UniqueID get uid;
 
   @JsonKey(ignore: true)
@@ -44,7 +71,19 @@ mixin _$Series {
 abstract class $SeriesCopyWith<$Res> {
   factory $SeriesCopyWith(Series value, $Res Function(Series) then) =
       _$SeriesCopyWithImpl<$Res>;
-  $Res call({bool isDeleted, bool isNSFW, UniqueID uid});
+  $Res call(
+      {UniqueID authorUID,
+      CoverURL coverURL,
+      String deletionReason,
+      Genre genre,
+      Genre genreOptional,
+      bool isDeleted,
+      bool isNSFW,
+      Language language,
+      Subtitle subtitle,
+      Summary summary,
+      Title title,
+      UniqueID uid});
 }
 
 /// @nodoc
@@ -57,13 +96,36 @@ class _$SeriesCopyWithImpl<$Res> implements $SeriesCopyWith<$Res> {
 
   @override
   $Res call({
+    Object authorUID = freezed,
+    Object coverURL = freezed,
+    Object deletionReason = freezed,
+    Object genre = freezed,
+    Object genreOptional = freezed,
     Object isDeleted = freezed,
     Object isNSFW = freezed,
+    Object language = freezed,
+    Object subtitle = freezed,
+    Object summary = freezed,
+    Object title = freezed,
     Object uid = freezed,
   }) {
     return _then(_value.copyWith(
+      authorUID:
+          authorUID == freezed ? _value.authorUID : authorUID as UniqueID,
+      coverURL: coverURL == freezed ? _value.coverURL : coverURL as CoverURL,
+      deletionReason: deletionReason == freezed
+          ? _value.deletionReason
+          : deletionReason as String,
+      genre: genre == freezed ? _value.genre : genre as Genre,
+      genreOptional: genreOptional == freezed
+          ? _value.genreOptional
+          : genreOptional as Genre,
       isDeleted: isDeleted == freezed ? _value.isDeleted : isDeleted as bool,
       isNSFW: isNSFW == freezed ? _value.isNSFW : isNSFW as bool,
+      language: language == freezed ? _value.language : language as Language,
+      subtitle: subtitle == freezed ? _value.subtitle : subtitle as Subtitle,
+      summary: summary == freezed ? _value.summary : summary as Summary,
+      title: title == freezed ? _value.title : title as Title,
       uid: uid == freezed ? _value.uid : uid as UniqueID,
     ));
   }
@@ -74,7 +136,19 @@ abstract class _$SeriesCopyWith<$Res> implements $SeriesCopyWith<$Res> {
   factory _$SeriesCopyWith(_Series value, $Res Function(_Series) then) =
       __$SeriesCopyWithImpl<$Res>;
   @override
-  $Res call({bool isDeleted, bool isNSFW, UniqueID uid});
+  $Res call(
+      {UniqueID authorUID,
+      CoverURL coverURL,
+      String deletionReason,
+      Genre genre,
+      Genre genreOptional,
+      bool isDeleted,
+      bool isNSFW,
+      Language language,
+      Subtitle subtitle,
+      Summary summary,
+      Title title,
+      UniqueID uid});
 }
 
 /// @nodoc
@@ -88,13 +162,36 @@ class __$SeriesCopyWithImpl<$Res> extends _$SeriesCopyWithImpl<$Res>
 
   @override
   $Res call({
+    Object authorUID = freezed,
+    Object coverURL = freezed,
+    Object deletionReason = freezed,
+    Object genre = freezed,
+    Object genreOptional = freezed,
     Object isDeleted = freezed,
     Object isNSFW = freezed,
+    Object language = freezed,
+    Object subtitle = freezed,
+    Object summary = freezed,
+    Object title = freezed,
     Object uid = freezed,
   }) {
     return _then(_Series(
+      authorUID:
+          authorUID == freezed ? _value.authorUID : authorUID as UniqueID,
+      coverURL: coverURL == freezed ? _value.coverURL : coverURL as CoverURL,
+      deletionReason: deletionReason == freezed
+          ? _value.deletionReason
+          : deletionReason as String,
+      genre: genre == freezed ? _value.genre : genre as Genre,
+      genreOptional: genreOptional == freezed
+          ? _value.genreOptional
+          : genreOptional as Genre,
       isDeleted: isDeleted == freezed ? _value.isDeleted : isDeleted as bool,
       isNSFW: isNSFW == freezed ? _value.isNSFW : isNSFW as bool,
+      language: language == freezed ? _value.language : language as Language,
+      subtitle: subtitle == freezed ? _value.subtitle : subtitle as Subtitle,
+      summary: summary == freezed ? _value.summary : summary as Summary,
+      title: title == freezed ? _value.title : title as Title,
       uid: uid == freezed ? _value.uid : uid as UniqueID,
     ));
   }
@@ -103,32 +200,95 @@ class __$SeriesCopyWithImpl<$Res> extends _$SeriesCopyWithImpl<$Res>
 /// @nodoc
 class _$_Series implements _Series {
   _$_Series(
-      {@required this.isDeleted, @required this.isNSFW, @required this.uid})
-      : assert(isDeleted != null),
+      {@required this.authorUID,
+      @required this.coverURL,
+      @required this.deletionReason,
+      @required this.genre,
+      @required this.genreOptional,
+      @required this.isDeleted,
+      @required this.isNSFW,
+      @required this.language,
+      @required this.subtitle,
+      @required this.summary,
+      @required this.title,
+      @required this.uid})
+      : assert(authorUID != null),
+        assert(coverURL != null),
+        assert(deletionReason != null),
+        assert(genre != null),
+        assert(genreOptional != null),
+        assert(isDeleted != null),
         assert(isNSFW != null),
+        assert(language != null),
+        assert(subtitle != null),
+        assert(summary != null),
+        assert(title != null),
         assert(uid != null);
 
+  @override
+  final UniqueID authorUID;
+  @override
+  final CoverURL coverURL;
+  @override
+  final String deletionReason;
+  @override
+  final Genre genre;
+  @override
+  final Genre genreOptional;
   @override
   final bool isDeleted;
   @override
   final bool isNSFW;
   @override
+  final Language language;
+  @override
+  final Subtitle subtitle;
+  @override
+  final Summary summary;
+  @override
+  final Title title;
+  @override
   final UniqueID uid;
 
   @override
   String toString() {
-    return 'Series(isDeleted: $isDeleted, isNSFW: $isNSFW, uid: $uid)';
+    return 'Series(authorUID: $authorUID, coverURL: $coverURL, deletionReason: $deletionReason, genre: $genre, genreOptional: $genreOptional, isDeleted: $isDeleted, isNSFW: $isNSFW, language: $language, subtitle: $subtitle, summary: $summary, title: $title, uid: $uid)';
   }
 
   @override
   bool operator ==(dynamic other) {
     return identical(this, other) ||
         (other is _Series &&
+            (identical(other.authorUID, authorUID) ||
+                const DeepCollectionEquality()
+                    .equals(other.authorUID, authorUID)) &&
+            (identical(other.coverURL, coverURL) ||
+                const DeepCollectionEquality()
+                    .equals(other.coverURL, coverURL)) &&
+            (identical(other.deletionReason, deletionReason) ||
+                const DeepCollectionEquality()
+                    .equals(other.deletionReason, deletionReason)) &&
+            (identical(other.genre, genre) ||
+                const DeepCollectionEquality().equals(other.genre, genre)) &&
+            (identical(other.genreOptional, genreOptional) ||
+                const DeepCollectionEquality()
+                    .equals(other.genreOptional, genreOptional)) &&
             (identical(other.isDeleted, isDeleted) ||
                 const DeepCollectionEquality()
                     .equals(other.isDeleted, isDeleted)) &&
             (identical(other.isNSFW, isNSFW) ||
                 const DeepCollectionEquality().equals(other.isNSFW, isNSFW)) &&
+            (identical(other.language, language) ||
+                const DeepCollectionEquality()
+                    .equals(other.language, language)) &&
+            (identical(other.subtitle, subtitle) ||
+                const DeepCollectionEquality()
+                    .equals(other.subtitle, subtitle)) &&
+            (identical(other.summary, summary) ||
+                const DeepCollectionEquality()
+                    .equals(other.summary, summary)) &&
+            (identical(other.title, title) ||
+                const DeepCollectionEquality().equals(other.title, title)) &&
             (identical(other.uid, uid) ||
                 const DeepCollectionEquality().equals(other.uid, uid)));
   }
@@ -136,8 +296,17 @@ class _$_Series implements _Series {
   @override
   int get hashCode =>
       runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(authorUID) ^
+      const DeepCollectionEquality().hash(coverURL) ^
+      const DeepCollectionEquality().hash(deletionReason) ^
+      const DeepCollectionEquality().hash(genre) ^
+      const DeepCollectionEquality().hash(genreOptional) ^
       const DeepCollectionEquality().hash(isDeleted) ^
       const DeepCollectionEquality().hash(isNSFW) ^
+      const DeepCollectionEquality().hash(language) ^
+      const DeepCollectionEquality().hash(subtitle) ^
+      const DeepCollectionEquality().hash(summary) ^
+      const DeepCollectionEquality().hash(title) ^
       const DeepCollectionEquality().hash(uid);
 
   @JsonKey(ignore: true)
@@ -148,14 +317,41 @@ class _$_Series implements _Series {
 
 abstract class _Series implements Series {
   factory _Series(
-      {@required bool isDeleted,
+      {@required UniqueID authorUID,
+      @required CoverURL coverURL,
+      @required String deletionReason,
+      @required Genre genre,
+      @required Genre genreOptional,
+      @required bool isDeleted,
       @required bool isNSFW,
+      @required Language language,
+      @required Subtitle subtitle,
+      @required Summary summary,
+      @required Title title,
       @required UniqueID uid}) = _$_Series;
 
+  @override
+  UniqueID get authorUID;
+  @override
+  CoverURL get coverURL;
+  @override
+  String get deletionReason;
+  @override
+  Genre get genre;
+  @override
+  Genre get genreOptional;
   @override
   bool get isDeleted;
   @override
   bool get isNSFW;
+  @override
+  Language get language;
+  @override
+  Subtitle get subtitle;
+  @override
+  Summary get summary;
+  @override
+  Title get title;
   @override
   UniqueID get uid;
   @override
