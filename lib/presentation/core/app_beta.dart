5ccb1a302b29e68d9a17bb95de909172f69b27ea
François Grèzes <grezesfrancois@gmail.com>
2021-04-22 20:00:11 +0200

    add authbloc + homebloc to global scope

--- lib/presentation/core/app_beta.dart
+++ lib/presentation/core/app_beta.dart
@@ -1,6 +1,10 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:wine/application/auth/auth_bloc.dart';
+import 'package:wine/application/home/home_bloc.dart';
 import 'package:wine/flavors.dart';
+import 'package:wine/injection.dart';
 import 'package:wine/presentation/routes/router.gr.dart';
 import 'package:wine/presentation/splash/splash_page.dart';
 
@@ -14,16 +18,26 @@ class AppBeta extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      builder: ExtendedNavigator.builder<AppRouter>(
-        router: AppRouter(),
-        // observers: <NavigatorObserver>[
-        //   FirebaseAnalyticsObserver(analytics: analytics),
-        // ],
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => getIt<AuthBloc>(),
+        ),
+        BlocProvider(
+          create: (_) => getIt<HomeBloc>(),
+        ),
+      ],
+      child: MaterialApp(
+        builder: ExtendedNavigator.builder<AppRouter>(
+          router: AppRouter(),
+          // observers: <NavigatorObserver>[
+          //   FirebaseAnalyticsObserver(analytics: analytics),
+          // ],
+        ),
+        debugShowCheckedModeBanner: false,
+        home: SplashPage(),
+        title: F.title,
       ),
-      debugShowCheckedModeBanner: false,
-      home: SplashPage(),
-      title: F.title,
     );
   }
 }
--- lib/presentation/core/app_development.dart
+++ lib/presentation/core/app_development.dart
@@ -1,6 +1,10 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:wine/application/auth/auth_bloc.dart';
+import 'package:wine/application/home/home_bloc.dart';
 import 'package:wine/flavors.dart';
+import 'package:wine/injection.dart';
 import 'package:wine/presentation/routes/router.gr.dart';
 import 'package:wine/presentation/splash/splash_page.dart';
 
@@ -11,10 +15,20 @@ class AppDevelopment extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      builder: ExtendedNavigator.builder<AppRouter>(router: AppRouter()),
-      home: SplashPage(),
-      title: F.title,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => getIt<AuthBloc>(),
+        ),
+        BlocProvider(
+          create: (_) => getIt<HomeBloc>(),
+        ),
+      ],
+      child: MaterialApp(
+        builder: ExtendedNavigator.builder<AppRouter>(router: AppRouter()),
+        home: SplashPage(),
+        title: F.title,
+      ),
     );
   }
 }
