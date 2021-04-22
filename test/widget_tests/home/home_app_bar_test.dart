4b0139d48a9283230de059f82e6293666faa1f60
François Grèzes <grezesfrancois@gmail.com>
2021-04-22 22:51:46 +0200

    first test pass

--- test/widget_tests/home/home_app_bar_test.dart
+++ test/widget_tests/home/home_app_bar_test.dart
@@ -1,16 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:wine/application/auth/auth_bloc.dart';
+import 'package:wine/domain/auth/i_auth_facade.dart';
 import 'package:wine/presentation/home/widgets/home_app_bar.dart';
 
+import '../../mocks/auth_mocks.dart';
 import '../utils/main_widget.dart';
 
 void main() {
   group('HomeAppBar -', () {
+    IAuthFacade _authFacade;
+
+    setUp(() {
+      _authFacade = MockAuthFacade();
+    });
+
     testWidgets(
       'Should not find new Series button',
       (WidgetTester tester) async {
+        when(() => _authFacade.isLoggedIn).thenReturn(true);
+        when(() => _authFacade.isAnonymous).thenReturn(true);
+
         await tester.pumpWidget(MainWidget(
-          child: HomeAppBar(),
+          child: BlocProvider(
+            create: (context) => AuthBloc(_authFacade),
+            child: HomeAppBar(),
+          ),
         ));
 
         final filterButton = find.byKey(const Key('filter_button'));
