709d54651cf8386881d834eba60c9d8aaae661ad
François Grèzes <grezesfrancois@gmail.com>
2021-04-25 17:30:13 +0200

    add home_menu_layout + tests

---
+++ lib/presentation/home/home_menu_layout.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:simple_gesture_detector/simple_gesture_detector.dart';
+import 'package:wine/application/auth/auth_bloc.dart';
+import 'package:wine/application/home/home_navigation/home_navigation_bloc.dart';
+import 'package:wine/presentation/core/buttons/animated_button.dart';
+import 'package:wine/presentation/home/widgets/home_menu_tile.dart';
+import 'package:wine/utils/assets/animations.dart';
+
+/// @nodoc
+class HomeMenuLayout extends StatelessWidget {
+  /// @nodoc
+  const HomeMenuLayout({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context).size;
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        brightness: Brightness.light,
+        elevation: 0.0,
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(right: 15.0),
+            child: BlocBuilder<HomeNavigationBloc, HomeNavigationState>(
+              builder: (context, homeNavigationState) {
+                return AnimatedButton(
+                  key: const Key('menu_button_open'),
+                  animation: homeNavigationState.isRightDrawerOpen
+                      ? 'menu_to_x'
+                      : 'x_to_menu',
+                  filename: menuAnimation,
+                  onPressed: () => context
+                      .read<HomeNavigationBloc>()
+                      .add(const HomeNavigationEvent.rightDrawerIconPressed()),
+                );
+              },
+            ),
+          )
+        ],
+      ),
+      body: SafeArea(
+        child: Stack(
+          children: <Widget>[
+            SimpleGestureDetector(
+              onHorizontalSwipe: (direction) {
+                if (direction == SwipeDirection.right) {}
+              },
+              child: Container(
+                color: Colors.transparent,
+                width: mediaQuery.width,
+                height: mediaQuery.height,
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  BlocBuilder<AuthBloc, AuthState>(
+                    builder: (context, state) {
+                      return state.maybeMap(
+                        authenticated: (_) => HomeMenuTile(
+                          key: const Key('home_menu_library_tile'),
+                          onPressed: () {},
+                          text: 'LIBRARY',
+                        ),
+                        orElse: () => Container(),
+                      );
+                    },
+                  ),
+                  const SizedBox(height: 25),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 50.0),
+                    child: HomeMenuTile(
+                      key: const Key('home_menu_plus_tile'),
+                      onPressed: () {},
+                      text: 'PLUS',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ test/widget_tests/home/home_menu_layout_test.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:wine/application/auth/auth_bloc.dart';
+import 'package:wine/application/home/home_navigation/home_navigation_bloc.dart';
+import 'package:wine/domain/auth/i_auth_facade.dart';
+import 'package:wine/presentation/home/home_menu_layout.dart';
+
+import '../../mocks/auth_mocks.dart';
+import '../utils/main_widget.dart';
+
+void main() {
+  group('HomeMenuLayout -', () {
+    IAuthFacade _authFacade;
+
+    setUp(() {
+      _authFacade = MockAuthFacade();
+    });
+
+    testWidgets(
+      'Should not display LIBRARY button',
+      (WidgetTester tester) async {
+        when(() => _authFacade.isLoggedIn).thenReturn(true);
+        when(() => _authFacade.isAnonymous).thenReturn(true);
+
+        await tester.pumpWidget(MainWidget(
+          child: MultiBlocProvider(
+            providers: <BlocProvider>[
+              BlocProvider<AuthBloc>(
+                create: (context) => AuthBloc(_authFacade),
+              ),
+              BlocProvider<HomeNavigationBloc>(
+                create: (context) => HomeNavigationBloc(),
+              ),
+            ],
+            child: const HomeMenuLayout(),
+          ),
+        ));
+        await tester.pump();
+
+        final libraryMenuTile = find.byKey(const Key('home_menu_library_tile'));
+        final plusMenuTile = find.byKey(const Key('home_menu_plus_tile'));
+
+        expect(libraryMenuTile, findsNothing);
+        expect(plusMenuTile, findsOneWidget);
+      },
+    );
+
+    testWidgets(
+      'Should display LIBRARY button',
+      (WidgetTester tester) async {
+        when(() => _authFacade.isLoggedIn).thenReturn(true);
+        when(() => _authFacade.isAnonymous).thenReturn(true);
+
+        await tester.pumpWidget(MainWidget(
+          child: MultiBlocProvider(
+            providers: <BlocProvider>[
+              BlocProvider<AuthBloc>(
+                create: (context) =>
+                    AuthBloc(_authFacade)..add(const AuthEvent.authChanged()),
+              ),
+              BlocProvider<HomeNavigationBloc>(
+                create: (context) => HomeNavigationBloc(),
+              ),
+            ],
+            child: const HomeMenuLayout(),
+          ),
+        ));
+        await tester.pump();
+
+        final libraryMenuTile = find.byKey(const Key('home_menu_library_tile'));
+        final plusMenuTile = find.byKey(const Key('home_menu_plus_tile'));
+
+        expect(libraryMenuTile, findsNothing);
+        expect(plusMenuTile, findsOneWidget);
+      },
+    );
+  });
+}
