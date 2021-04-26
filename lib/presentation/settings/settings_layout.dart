69654bfd08db1fa696bccd0d0a1164cd5a7ac1e2
François Grèzes <grezesfrancois@gmail.com>
2021-04-26 23:12:40 +0200

    add settings_page + layout

---
+++ lib/presentation/settings/settings_layout.dart
@@ -0,0 +1,89 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:wine/application/auth/auth_bloc.dart';
+import 'package:wine/presentation/core/buttons/asset_button.dart';
+import 'package:wine/presentation/core/buttons/tile_button.dart';
+import 'package:wine/utils/assets/icons.dart';
+
+/// @nodoc
+class SettingsLayout extends StatelessWidget {
+  /// @nodoc
+  const SettingsLayout({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(41.5),
+        child: AppBar(
+          backgroundColor: Colors.transparent,
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(0.0),
+            child: Container(
+              color: Colors.black,
+              height: 2.0,
+            ),
+          ),
+          brightness: Brightness.light,
+          centerTitle: true,
+          elevation: 0.0,
+          leading: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 10.0,
+              vertical: 5.0,
+            ),
+            child: AssetButton(
+              imagePath: backIcon,
+              onPressed: () async => ExtendedNavigator.root.pop(),
+            ),
+          ),
+          title: const Text(
+            'SETTINGS',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 18.0,
+              fontWeight: FontWeight.w300,
+            ),
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: ListView(
+          children: <Widget>[
+            BlocBuilder<AuthBloc, AuthState>(
+              builder: (context, state) {
+                return TileButton(
+                  leadingIconData: Icons.account_circle_outlined,
+                  title: 'ACCOUNT',
+                  trailingIconData: Icons.keyboard_arrow_right,
+                  onPressed: () => state.maybeMap(
+                    authenticated: (_) async {},
+                    // TODO add push to account settings page
+                    orElse: () async {},
+                  ),
+                );
+              },
+            ),
+            TileButton(
+              leadingIconData: Feather.book,
+              title: 'SERIES',
+              trailingIconData: Icons.keyboard_arrow_right,
+              // TODO add push series settings page
+              onPressed: () {},
+            ),
+            TileButton(
+              leadingIconData: Feather.book_open,
+              title: 'CHAPTER',
+              trailingIconData: Icons.keyboard_arrow_right,
+              // TODO add push chapter settings page
+              onPressed: () {},
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/presentation/settings/settings_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:wine/presentation/settings/settings_layout.dart';
+import 'package:wine/utils/themes.dart';
+
+/// @nodoc
+class SettingsPage extends StatelessWidget {
+  /// @nodoc
+  const SettingsPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: lightTheme,
+      child: const SettingsLayout(),
+    );
+  }
+}
