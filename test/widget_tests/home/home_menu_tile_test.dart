dd0571b93770019e61fac518e55e398a417cfeb1
François Grèzes <grezesfrancois@gmail.com>
2021-04-27 22:58:26 +0200

    update home_menu_tile tests

--- test/widget_tests/home/home_menu_tile_test.dart
+++ test/widget_tests/home/home_menu_tile_test.dart
@@ -8,7 +8,22 @@ import '../utils/main_widget.dart';
 
 void main() {
   group('HomeMenuTile -', () {
-    testWidgets('Should be true', (WidgetTester tester) async {
+    testWidgets('Should find text Hello World!', (tester) async {
+      final completer = Completer<void>();
+
+      await tester.pumpWidget(MainWidget(
+        child: HomeMenuTile(
+          key: const Key('test_home_menu_tile'),
+          onPressed: completer.complete,
+          text: 'Hello World!',
+        ),
+      ));
+      await tester.pump();
+
+      expect(find.text('Hello World!'), findsOneWidget);
+    });
+
+    testWidgets('completer be true', (tester) async {
       final completer = Completer<void>();
 
       await tester.pumpWidget(MainWidget(
@@ -25,7 +40,6 @@ void main() {
       await tester.tap(homeMenuTile);
       await tester.pump();
 
-      expect(find.text('Hello World!'), findsOneWidget);
       expect(completer.isCompleted, true);
     });
   });
