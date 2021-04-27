4078b7367c7d1d88ae82b9725fdade7251278662
François Grèzes <grezesfrancois@gmail.com>
2021-04-27 22:33:42 +0200

    update asset button test description

--- test/widget_tests/core/buttons/asset_button_test.dart
+++ test/widget_tests/core/buttons/asset_button_test.dart
@@ -9,23 +9,26 @@ import '../../utils/main_widget.dart';
 
 void main() {
   group('AssetButton -', () {
-    testWidgets('Should be true', (WidgetTester tester) async {
-      final completer = Completer<void>();
+    testWidgets(
+      'When pressed Then completer is true',
+      (WidgetTester tester) async {
+        final completer = Completer<void>();
 
-      await tester.pumpWidget(MainWidget(
-        child: AssetButton(
-          imagePath: plusIcon,
-          key: const Key('test_asset_button'),
-          onPressed: completer.complete,
-        ),
-      ));
+        await tester.pumpWidget(MainWidget(
+          child: AssetButton(
+            imagePath: plusIcon,
+            key: const Key('test_asset_button'),
+            onPressed: completer.complete,
+          ),
+        ));
 
-      final assetButton = find.byKey(const Key('test_asset_button'));
+        final assetButton = find.byKey(const Key('test_asset_button'));
 
-      await tester.tap(assetButton);
-      await tester.pump();
+        await tester.tap(assetButton);
+        await tester.pump();
 
-      expect(completer.isCompleted, isTrue);
-    });
+        expect(completer.isCompleted, isTrue);
+      },
+    );
   });
 }
