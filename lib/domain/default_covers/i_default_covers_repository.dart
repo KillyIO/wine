ccf16ef1b4c1014a2e07f065ec9af2d621e6d652
François Grèzes <grezesfrancois@gmail.com>
2021-05-01 19:27:04 +0200

    update default covers interface with rustic + null safety

--- lib/domain/default_covers/i_default_covers_repository.dart
+++ lib/domain/default_covers/i_default_covers_repository.dart
@@ -1,19 +1,20 @@
-import 'package:dartz/dartz.dart';
+import 'package:rustic/result.dart';
+import 'package:rustic/tuple.dart';
 import 'package:wine/domain/default_covers/default_covers_failure.dart';
 
 /// @nodoc
 abstract class IDefaultCoversRepository {
   /// @nodoc
-  Future<Either<DefaultCoversFailure, Unit>> cacheDefaultCoverURLs(
+  Future<Result<DefaultCoversFailure, Unit>> cacheDefaultCoverURLs(
     Map<String, String> urls,
   );
 
   /// @nodoc
-  Future<Either<DefaultCoversFailure, String>> fetchDefaultCoverURLByKey(
+  Future<Result<DefaultCoversFailure, String>> fetchDefaultCoverURLByKey(
     String key,
   );
 
   /// @nodoc
-  Future<Either<DefaultCoversFailure, Map<String, String>>>
+  Future<Result<DefaultCoversFailure, Map<String, String>>>
       loadDefaultCoverURLs();
 }
