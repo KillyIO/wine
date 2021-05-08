0de4370666bdf86c0bdd637134f1e35d02e465da
François Grèzes <grezesfrancois@gmail.com>
2021-05-08 13:25:09 +0200

    add HiveUser mock

--- test/mocks/hive_mocks.dart
+++ test/mocks/hive_mocks.dart
@@ -1,6 +1,9 @@
 import 'package:hive/hive.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:wine/infrastructure/user/hive_user.dart';
 
 class MockHiveInterface extends Mock implements HiveInterface {}
 
 class MockBox<T> extends Mock implements Box<T> {}
+
+class MockHiveUser extends Mock implements HiveUser {}
