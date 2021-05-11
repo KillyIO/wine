be73494d4ceedb2b15f9ed247716906ae4e4f221
François Grèzes <grezesfrancois@gmail.com>
2021-05-11 19:41:09 +0200

    fix HomeMenuLayout tests wwith null safety

--- test/widget_tests/home/home_menu_layout_test.dart
+++ test/widget_tests/home/home_menu_layout_test.dart
@@ -7,74 +7,68 @@ import 'package:wine/application/home/home_navigation/home_navigation_bloc.dart'
 import 'package:wine/domain/auth/i_auth_facade.dart';
 import 'package:wine/presentation/home/home_menu_layout.dart';
 
-import '../../mocks/auth_mocks.dart';
+import '../../mocks/auth_facade_mocks.dart';
 import '../utils/main_widget.dart';
 
 void main() {
   group('HomeMenuLayout -', () {
-    IAuthFacade _authFacade;
+    late IAuthFacade _authFacade;
 
     setUp(() {
       _authFacade = MockAuthFacade();
     });
 
-    testWidgets(
-      'Should not display LIBRARY button',
-      (WidgetTester tester) async {
-        when(() => _authFacade.isLoggedIn).thenReturn(true);
-        when(() => _authFacade.isAnonymous).thenReturn(true);
+    testWidgets('Should not display LIBRARY button', (tester) async {
+      when(() => _authFacade.isLoggedIn).thenReturn(true);
+      when(() => _authFacade.isAnonymous).thenReturn(true);
 
-        await tester.pumpWidget(MainWidget(
-          child: MultiBlocProvider(
-            providers: <BlocProvider>[
-              BlocProvider<AuthBloc>(
-                create: (context) => AuthBloc(_authFacade),
-              ),
-              BlocProvider<HomeNavigationBloc>(
-                create: (context) => HomeNavigationBloc(),
-              ),
-            ],
-            child: const HomeMenuLayout(),
-          ),
-        ));
-        await tester.pump();
+      await tester.pumpWidget(MainWidget(
+        child: MultiBlocProvider(
+          providers: <BlocProvider>[
+            BlocProvider<AuthBloc>(
+              create: (context) => AuthBloc(_authFacade),
+            ),
+            BlocProvider<HomeNavigationBloc>(
+              create: (context) => HomeNavigationBloc(),
+            ),
+          ],
+          child: const HomeMenuLayout(),
+        ),
+      ));
+      await tester.pump();
 
-        final libraryMenuTile = find.byKey(const Key('home_menu_library_tile'));
-        final plusMenuTile = find.byKey(const Key('home_menu_plus_tile'));
+      final libraryMenuTile = find.byKey(const Key('home_menu_library_tile'));
+      final plusMenuTile = find.byKey(const Key('home_menu_plus_tile'));
 
-        expect(libraryMenuTile, findsNothing);
-        expect(plusMenuTile, findsOneWidget);
-      },
-    );
+      expect(libraryMenuTile, findsNothing);
+      expect(plusMenuTile, findsOneWidget);
+    });
 
-    testWidgets(
-      'Should display LIBRARY button',
-      (WidgetTester tester) async {
-        when(() => _authFacade.isLoggedIn).thenReturn(true);
-        when(() => _authFacade.isAnonymous).thenReturn(true);
+    testWidgets('Should display LIBRARY button', (tester) async {
+      when(() => _authFacade.isLoggedIn).thenReturn(true);
+      when(() => _authFacade.isAnonymous).thenReturn(true);
 
-        await tester.pumpWidget(MainWidget(
-          child: MultiBlocProvider(
-            providers: <BlocProvider>[
-              BlocProvider<AuthBloc>(
-                create: (context) =>
-                    AuthBloc(_authFacade)..add(const AuthEvent.authChanged()),
-              ),
-              BlocProvider<HomeNavigationBloc>(
-                create: (context) => HomeNavigationBloc(),
-              ),
-            ],
-            child: const HomeMenuLayout(),
-          ),
-        ));
-        await tester.pump();
+      await tester.pumpWidget(MainWidget(
+        child: MultiBlocProvider(
+          providers: <BlocProvider>[
+            BlocProvider<AuthBloc>(
+              create: (context) =>
+                  AuthBloc(_authFacade)..add(const AuthEvent.authChanged()),
+            ),
+            BlocProvider<HomeNavigationBloc>(
+              create: (context) => HomeNavigationBloc(),
+            ),
+          ],
+          child: const HomeMenuLayout(),
+        ),
+      ));
+      await tester.pump();
 
-        final libraryMenuTile = find.byKey(const Key('home_menu_library_tile'));
-        final plusMenuTile = find.byKey(const Key('home_menu_plus_tile'));
+      final libraryMenuTile = find.byKey(const Key('home_menu_library_tile'));
+      final plusMenuTile = find.byKey(const Key('home_menu_plus_tile'));
 
-        expect(libraryMenuTile, findsNothing);
-        expect(plusMenuTile, findsOneWidget);
-      },
-    );
+      expect(libraryMenuTile, findsNothing);
+      expect(plusMenuTile, findsOneWidget);
+    });
   });
 }
