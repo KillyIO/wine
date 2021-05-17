84f889b8cb40a4ba01cf25e71da4a06a83233664
François Grèzes <grezesfrancois@gmail.com>
2021-05-17 21:33:19 +0200

    replace SplashBloc for SetupBloc + fix errors with null safety

--- lib/utils/functions.dart
+++ lib/utils/functions.dart
@@ -2,22 +2,27 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:wine/application/setup/setup_bloc.dart';
 
-import 'package:wine/application/splash/splash_bloc.dart';
 import 'package:wine/presentation/core/dialogs/error_dialog.dart';
 
-/// Tries to restart the app by add initial [SplashBloc] event
+/// Tries to restart the app by add initial [SetupBloc] event
 Future<void> restartApp(BuildContext context, String errorMessage) async {
-  final result = await showDialog(
+  final result = await showDialog<bool>(
     context: context,
     builder: (_) => ErrorDialog(
+      key: const Key('setup_error_dialog'),
       message: errorMessage,
       buttonText: 'RESTART',
-      onPressed: () async => ExtendedNavigator.of(context).pop<bool>(true),
+      onPressed: () async {
+        if (context.router.canPopSelfOrChildren) {
+          await context.router.pop<bool>(true);
+        }
+      },
     ),
   );
 
-  if (result) {
-    context.read<SplashBloc>().add(const SplashEvent.splashPageLaunched());
+  if (result != null && result) {
+    context.read<SetupBloc>().add(const SetupEvent.appLaunched());
   }
 }
