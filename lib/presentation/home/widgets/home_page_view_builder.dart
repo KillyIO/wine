c7b4143cc2dfa6865e7b45b04592a180fef735d7
François Grèzes <grezesfrancois@gmail.com>
2021-05-25 20:55:26 +0200

    add basic pageview build

---
+++ lib/presentation/home/widgets/home_page_view_builder.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:wine/application/home/home_navigation/home_navigation_bloc.dart';
+import 'package:wine/injection.dart';
+
+/// @nodoc
+class HomePageViewBuilder extends StatelessWidget {
+  /// @nodoc
+  HomePageViewBuilder({
+    Key? key,
+    required this.controller,
+  }) : super(key: key);
+
+  /// @nodoc
+  final PageController controller;
+
+  final List<Widget> _pageViewLayouts = <Widget>[Container(), Container()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: PageView.builder(
+        controller: controller,
+        itemBuilder: (context, index) =>
+            _pageViewLayouts[index % _pageViewLayouts.length],
+        onPageChanged: (index) {
+          context.read<HomeNavigationBloc>().add(
+                HomeNavigationEvent.pageViewIndexChanged(
+                  index % _pageViewLayouts.length,
+                ),
+              );
+        },
+      ),
+    );
+  }
+}
