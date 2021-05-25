5ff5320dae3d5ad81271462ef5e61f742e479015
François Grèzes <grezesfrancois@gmail.com>
2021-05-25 20:54:44 +0200

    replace scaffold for drawer

--- lib/presentation/home/home_menu_layout.dart
+++ lib/presentation/home/home_menu_layout.dart
@@ -1,11 +1,9 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:simple_gesture_detector/simple_gesture_detector.dart';
+import 'package:line_icons/line_icons.dart';
 import 'package:wine/application/auth/auth_bloc.dart';
-import 'package:wine/application/home/home_navigation/home_navigation_bloc.dart';
-import 'package:wine/presentation/core/buttons/animated_button.dart';
 import 'package:wine/presentation/home/widgets/home_menu_tile.dart';
-import 'package:wine/utils/assets/animations.dart';
 
 /// @nodoc
 class HomeMenuLayout extends StatelessWidget {
@@ -16,79 +14,65 @@ class HomeMenuLayout extends StatelessWidget {
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context).size;
 
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        brightness: Brightness.light,
-        elevation: 0.0,
-        actions: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(right: 15.0),
-            child: BlocBuilder<HomeNavigationBloc, HomeNavigationState>(
-              builder: (context, homeNavigationState) {
-                return AnimatedButton(
-                  key: const Key('menu_button_open'),
-                  animation: homeNavigationState.isRightDrawerOpen
-                      ? 'menu_to_x'
-                      : 'x_to_menu',
-                  filename: menuAnimation,
-                  onPressed: () => context
-                      .read<HomeNavigationBloc>()
-                      .add(const HomeNavigationEvent.rightDrawerIconPressed()),
-                );
-              },
-            ),
-          )
-        ],
-      ),
-      body: SafeArea(
-        child: Stack(
-          children: <Widget>[
-            SimpleGestureDetector(
-              onHorizontalSwipe: (direction) {
-                if (direction == SwipeDirection.right) {
-                  context
-                      .read<HomeNavigationBloc>()
-                      .add(const HomeNavigationEvent.rightDrawerIconPressed());
-                }
-              },
-              child: Container(
-                color: Colors.transparent,
-                width: mediaQuery.width,
-                height: mediaQuery.height,
-              ),
-            ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  BlocBuilder<AuthBloc, AuthState>(
-                    builder: (context, state) {
-                      return state.maybeMap(
-                        authenticated: (_) => HomeMenuTile(
-                          key: const Key('home_menu_library_tile'),
-                          onPressed: () {},
-                          text: 'LIBRARY',
-                        ),
-                        orElse: () => Container(),
-                      );
-                    },
-                  ),
-                  const SizedBox(height: 25),
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 50.0),
-                    child: HomeMenuTile(
-                      key: const Key('home_menu_plus_tile'),
-                      onPressed: () {},
-                      text: 'PLUS',
+    return SizedBox(
+      width: mediaQuery.width,
+      child: Drawer(
+        child: SafeArea(
+          child: Stack(
+            children: <Widget>[
+              Align(
+                alignment: Alignment.topCenter,
+                child: AppBar(
+                  backgroundColor: Colors.transparent,
+                  brightness: Brightness.light,
+                  elevation: 0.0,
+                  actions: [
+                    Builder(
+                      builder: (context) {
+                        return IconButton(
+                          icon: const Icon(
+                            LineIcons.times,
+                            color: Colors.black,
+                            size: 30.0,
+                          ),
+                          onPressed: context.router.pop,
+                        );
+                      },
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 50.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      BlocBuilder<AuthBloc, AuthState>(
+                        builder: (context, state) {
+                          return state.maybeMap(
+                            authenticated: (_) => HomeMenuTile(
+                              key: const Key('home_menu_library_tile'),
+                              onPressed: () {},
+                              text: 'LIBRARY',
+                            ),
+                            orElse: () => Container(),
+                          );
+                        },
+                      ),
+                      const SizedBox(height: 25),
+                      HomeMenuTile(
+                        key: const Key('home_menu_plus_tile'),
+                        onPressed: () {},
+                        text: 'PLUS',
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
