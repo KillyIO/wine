a2077c3929d7ecd6d5db339cc31778cd6c89075f
François Grèzes <grezesfrancois@gmail.com>
2021-05-25 20:55:02 +0200

    remove useless tests

--- test/unit_tests/application/home_navigation_bloc_test.dart
+++ test/unit_tests/application/home_navigation_bloc_test.dart
@@ -20,45 +20,6 @@ void main() {
       expect: () => <HomeNavigationState>[],
     );
 
-    group('LeftDrawerIconPressed -', () {
-      blocTest(
-        'When isLeftDrawerOpen false Then yield isLeftDrawerOpen true',
-        build: () => _homeNavigationBloc,
-        act: (HomeNavigationBloc bloc) {
-          return bloc.add(const HomeNavigationEvent.leftDrawerIconPressed());
-        },
-        expect: () => <HomeNavigationState>[
-          const HomeNavigationState(
-            currentPageViewIdx: 0,
-            isLeftDrawerOpen: true,
-            isRightDrawerOpen: false,
-          ),
-        ],
-      );
-
-      blocTest(
-        'When isLeftDrawerOpen true Then yield isLeftDrawerOpen false',
-        build: () => _homeNavigationBloc,
-        act: (HomeNavigationBloc bloc) {
-          return bloc
-            ..add(const HomeNavigationEvent.leftDrawerIconPressed())
-            ..add(const HomeNavigationEvent.leftDrawerIconPressed());
-        },
-        expect: () => <HomeNavigationState>[
-          const HomeNavigationState(
-            currentPageViewIdx: 0,
-            isLeftDrawerOpen: true,
-            isRightDrawerOpen: false,
-          ),
-          const HomeNavigationState(
-            currentPageViewIdx: 0,
-            isLeftDrawerOpen: false,
-            isRightDrawerOpen: false,
-          ),
-        ],
-      );
-    });
-
     group('PageViewIndexChanged -', () {
       blocTest(
         'When pageViewIndexChanged 1 Then yield currentPageViewIdx 1',
@@ -67,50 +28,7 @@ void main() {
           return bloc.add(const HomeNavigationEvent.pageViewIndexChanged(1));
         },
         expect: () => <HomeNavigationState>[
-          const HomeNavigationState(
-            currentPageViewIdx: 1,
-            isLeftDrawerOpen: false,
-            isRightDrawerOpen: false,
-          ),
-        ],
-      );
-    });
-
-    group('RightDrawerIconPressed -', () {
-      blocTest(
-        'When isRightDrawerOpen false Then yield isRightDrawerOpen true',
-        build: () => _homeNavigationBloc,
-        act: (HomeNavigationBloc bloc) {
-          return bloc.add(const HomeNavigationEvent.rightDrawerIconPressed());
-        },
-        expect: () => <HomeNavigationState>[
-          const HomeNavigationState(
-            currentPageViewIdx: 0,
-            isLeftDrawerOpen: false,
-            isRightDrawerOpen: true,
-          ),
-        ],
-      );
-
-      blocTest(
-        'When isRightDrawerOpen true Then yield isRightDrawerOpen false',
-        build: () => _homeNavigationBloc,
-        act: (HomeNavigationBloc bloc) {
-          return bloc
-            ..add(const HomeNavigationEvent.rightDrawerIconPressed())
-            ..add(const HomeNavigationEvent.rightDrawerIconPressed());
-        },
-        expect: () => <HomeNavigationState>[
-          const HomeNavigationState(
-            currentPageViewIdx: 0,
-            isLeftDrawerOpen: false,
-            isRightDrawerOpen: true,
-          ),
-          const HomeNavigationState(
-            currentPageViewIdx: 0,
-            isLeftDrawerOpen: false,
-            isRightDrawerOpen: false,
-          ),
+          const HomeNavigationState(currentPageViewIdx: 1),
         ],
       );
     });
