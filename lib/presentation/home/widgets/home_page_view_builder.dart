1c671f047a4d840cc190f651073862db911e8476
François Grèzes <grezesfrancois@gmail.com>
2021-05-25 20:55:42 +0200

    remove unused import

--- lib/presentation/home/widgets/home_page_view_builder.dart
+++ lib/presentation/home/widgets/home_page_view_builder.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:wine/application/home/home_navigation/home_navigation_bloc.dart';
-import 'package:wine/injection.dart';
 
 /// @nodoc
 class HomePageViewBuilder extends StatelessWidget {
