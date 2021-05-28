60b5df3205b526c20451f8931972805a4bb7a2dd
François Grèzes <grezesfrancois@gmail.com>
2021-05-28 18:09:23 +0200

    add library + plus paths

--- lib/utils/paths/router.dart
+++ lib/utils/paths/router.dart
@@ -2,4 +2,10 @@
 const String homePath = '/';
 
 /// @nodoc
+const String libraryPath = '/library';
+
+/// @nodoc
 const String onboardingPath = '/onboarding';
+
+/// @nodoc
+const String plusPath = '/plus';
