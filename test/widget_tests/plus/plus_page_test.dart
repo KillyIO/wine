66eb5b9fb8097a3b77e1ceb053ed832e893c1aed
François Grèzes <grezesfrancois@gmail.com>
2021-05-29 18:32:57 +0200

    add tests description for later

--- test/widget_tests/plus/plus_page_test.dart
+++ test/widget_tests/plus/plus_page_test.dart
@@ -1,7 +1,11 @@
 void main() {
+  // TODO test PlusBanner found
+
   // TODO test LIBRARY button not display when anonymous
 
-  // TODO test LIBRARY button navigate to LibraryPage
+  // TODO test LIBRARY button navigate to LibraryPage when authenticated
+
+  // TODO test LIBRARY button navigate to LogInPage when  anonymous
 
   // TODO test SETTINGS button navigate to SettingsPage
 
