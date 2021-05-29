05d218b60678c2ee7e455ecd9719be42caa1c2d4
François Grèzes <grezesfrancois@gmail.com>
2021-05-29 18:32:07 +0200

    add TODO for settings page + layout

--- test/widget_tests/settings/settings_layout_test.dart
+++ test/widget_tests/settings/settings_layout_test.dart
@@ -2,6 +2,10 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('SettingsLayout -', () {
-    // TODO add tests
+    // TODO test find ACCOUNT button
+
+    // TODO test find SERIES button
+
+    // TODO test find CHAPTER button
   });
 }
--- test/widget_tests/settings/settings_page_test.dart
+++ test/widget_tests/settings/settings_page_test.dart
@@ -1,1 +1,11 @@
-void main() {}
+void main() {
+  // TODO test find test SETTINGS
+
+  // TODO test when anonymous ACCOUNT button navigate to LogInPage
+
+  // TODO test when authenticated ACCOUNT button navigate to AccountPage
+
+  // TODO test SERIES button navigate to SeriesSettingsPage
+
+  // TODO test CHAPTER button navigate to ChapterSettingsPage
+}
