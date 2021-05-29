a7f18884b0a9946b5bd80e13a31a159febb4bff9
François Grèzes <grezesfrancois@gmail.com>
2021-05-29 18:33:36 +0200

    move ```Scaffold``` in page instead of layout

--- lib/presentation/settings/settings_layout.dart
+++ lib/presentation/settings/settings_layout.dart
@@ -3,9 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:line_icons/line_icons.dart';
 import 'package:wine/application/auth/auth_bloc.dart';
-import 'package:wine/presentation/core/buttons/asset_button.dart';
 import 'package:wine/presentation/core/buttons/tile_button.dart';
-import 'package:wine/utils/assets/icons.dart';
+import 'package:wine/utils/paths/router.dart';
 
 /// @nodoc
 class SettingsLayout extends StatelessWidget {
@@ -14,75 +13,38 @@ class SettingsLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(41.5),
-        child: AppBar(
-          backgroundColor: Colors.transparent,
-          bottom: PreferredSize(
-            preferredSize: const Size.fromHeight(0.0),
-            child: Container(
-              color: Colors.black,
-              height: 2.0,
-            ),
+    return SafeArea(
+      child: ListView(
+        children: <Widget>[
+          BlocBuilder<AuthBloc, AuthState>(
+            builder: (context, state) {
+              return TileButton(
+                leadingIconData: Icons.account_circle_outlined,
+                title: 'ACCOUNT',
+                trailingIconData: Icons.keyboard_arrow_right,
+                onPressed: () => state.maybeMap(
+                  authenticated: (_) async {},
+                  // TODO add push to account settings page
+                  orElse: () => context.router.root.pushNamed(logInPath),
+                ),
+              );
+            },
           ),
-          brightness: Brightness.light,
-          centerTitle: true,
-          elevation: 0.0,
-          leading: Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 10.0,
-              vertical: 5.0,
-            ),
-            child: AssetButton(
-              imagePath: backIcon,
-              onPressed: () async => context.router.pop(),
-            ),
+          TileButton(
+            leadingIconData: LineIcons.book,
+            title: 'SERIES',
+            trailingIconData: Icons.keyboard_arrow_right,
+            // TODO add push series settings page
+            onPressed: () {},
           ),
-          title: const Text(
-            'SETTINGS',
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 18.0,
-              fontWeight: FontWeight.w300,
-            ),
-          ),
-        ),
-      ),
-      body: SafeArea(
-        child: ListView(
-          children: <Widget>[
-            BlocBuilder<AuthBloc, AuthState>(
-              builder: (context, state) {
-                return TileButton(
-                  leadingIconData: Icons.account_circle_outlined,
-                  title: 'ACCOUNT',
-                  trailingIconData: Icons.keyboard_arrow_right,
-                  onPressed: () => state.maybeMap(
-                    authenticated: (_) async {},
-                    // TODO add push to account settings page
-                    orElse: () async {},
-                  ),
-                );
-              },
-            ),
-            TileButton(
-              leadingIconData: LineIcons.book,
-              title: 'SERIES',
-              trailingIconData: Icons.keyboard_arrow_right,
-              // TODO add push series settings page
-              onPressed: () {},
-            ),
-            TileButton(
-              leadingIconData: LineIcons.bookOpen,
-              title: 'CHAPTER',
-              trailingIconData: Icons.keyboard_arrow_right,
-              // TODO add push chapter settings page
-              onPressed: () {},
-            )
-          ],
-        ),
+          TileButton(
+            leadingIconData: LineIcons.bookOpen,
+            title: 'CHAPTER',
+            trailingIconData: Icons.keyboard_arrow_right,
+            // TODO add push chapter settings page
+            onPressed: () {},
+          )
+        ],
       ),
     );
   }
--- lib/presentation/settings/settings_page.dart
+++ lib/presentation/settings/settings_page.dart
@@ -1,6 +1,10 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+
+import 'package:wine/presentation/core/buttons/asset_button.dart';
 import 'package:wine/presentation/settings/settings_layout.dart';
+import 'package:wine/utils/assets/icons.dart';
 import 'package:wine/utils/themes.dart';
 
 /// @nodoc
@@ -16,7 +20,44 @@ class SettingsPage extends StatelessWidget {
     ]);
     return AnnotatedRegion<SystemUiOverlayStyle>(
       value: lightTheme,
-      child: const SettingsLayout(),
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(41.5),
+          child: AppBar(
+            backgroundColor: Colors.transparent,
+            bottom: PreferredSize(
+              preferredSize: const Size.fromHeight(0.0),
+              child: Container(
+                color: Colors.black,
+                height: 2.0,
+              ),
+            ),
+            brightness: Brightness.light,
+            centerTitle: true,
+            elevation: 0.0,
+            leading: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10.0,
+                vertical: 5.0,
+              ),
+              child: AssetButton(
+                imagePath: backIcon,
+                onPressed: context.router.pop,
+              ),
+            ),
+            title: const Text(
+              'SETTINGS',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 18.0,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+          ),
+        ),
+        body: const SettingsLayout(),
+      ),
     );
   }
 }
