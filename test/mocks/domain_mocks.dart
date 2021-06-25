c1f352306e0a8aa79508d2dc2e30047de4fbd8e9
François Grèzes <grezesfrancois@gmail.com>
2021-06-25 23:59:09 +0200

    add EmailAddress + Password mocks

--- test/mocks/domain_mocks.dart
+++ test/mocks/domain_mocks.dart
@@ -1,4 +1,10 @@
 import 'package:mocktail/mocktail.dart';
+import 'package:wine/domain/auth/email_address.dart';
+import 'package:wine/domain/auth/password.dart';
 import 'package:wine/domain/user/user.dart';
+
+class MockEmailAddress extends Mock implements EmailAddress {}
+
+class MockPassword extends Mock implements Password {}
 
 class MockUser extends Mock implements User {}
