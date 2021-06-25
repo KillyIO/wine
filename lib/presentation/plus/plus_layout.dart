1513edbb9e9274175069eb6f526cb948f26b9fa1
François Grèzes <grezesfrancois@gmail.com>
2021-06-25 23:58:44 +0200

    move Scaffold to plus_page

--- lib/presentation/plus/plus_layout.dart
+++ lib/presentation/plus/plus_layout.dart
@@ -3,10 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:line_icons/line_icons.dart';
 import 'package:wine/application/auth/auth_bloc.dart';
-import 'package:wine/presentation/core/buttons/asset_button.dart';
 import 'package:wine/presentation/plus/widgets/plus_banner.dart';
 import 'package:wine/presentation/core/buttons/tile_button.dart';
-import 'package:wine/utils/assets/icons.dart';
 import 'package:wine/utils/paths/router.dart';
 
 /// @nodoc
@@ -16,75 +14,40 @@ class PlusLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(41.5),
-        child: AppBar(
-          backgroundColor: Colors.transparent,
-          bottom: PreferredSize(
-            preferredSize: const Size.fromHeight(0.0),
-            child: Container(color: Colors.black, height: 2.0),
-          ),
-          brightness: Brightness.light,
-          centerTitle: true,
-          elevation: 0.0,
-          leading: Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 10.0,
-              vertical: 5.0,
-            ),
-            child: AssetButton(
-              key: const Key('plus_layout_back_button'),
-              imagePath: backIcon,
-              onPressed: () async => context.router.pop(),
-            ),
-          ),
-          title: const Text(
-            'PLUS',
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 18.0,
-              fontWeight: FontWeight.w300,
-            ),
-          ),
+    return Column(
+      children: <Widget>[
+        const PlusBanner(),
+        BlocBuilder<AuthBloc, AuthState>(
+          builder: (context, state) {
+            return TileButton(
+              key: const Key('plus_library_button'),
+              isFirst: true,
+              leadingIconData: Icons.collections_bookmark_outlined,
+              title: 'LIBRARY',
+              trailingIconData: Icons.keyboard_arrow_right,
+              onPressed: () => state.maybeMap(
+                authenticated: (_) =>
+                    context.router.root.pushNamed(libraryPath),
+                orElse: () => context.router.root.pushNamed(logInPath),
+              ),
+            );
+          },
         ),
-      ),
-      body: Column(
-        children: <Widget>[
-          const PlusBanner(),
-          BlocBuilder<AuthBloc, AuthState>(
-            builder: (context, state) {
-              return TileButton(
-                key: const Key('plus_library_button'),
-                isFirst: true,
-                leadingIconData: Icons.collections_bookmark_outlined,
-                title: 'LIBRARY',
-                trailingIconData: Icons.keyboard_arrow_right,
-                onPressed: () => state.maybeMap(
-                  authenticated: (_) =>
-                      context.router.root.pushNamed(libraryPath),
-                  orElse: () async {},
-                ),
-              );
-            },
-          ),
-          TileButton(
-            key: const Key('plus_settings_button'),
-            leadingIconData: LineIcons.cog,
-            onPressed: () {},
-            title: 'SETTINGS',
-            trailingIconData: Icons.keyboard_arrow_right,
-          ),
-          TileButton(
-            key: const Key('plus_about_button'),
-            leadingIconData: Icons.info_outline_rounded,
-            onPressed: () {},
-            title: 'ABOUT',
-            trailingIconData: Icons.keyboard_arrow_right,
-          ),
-        ],
-      ),
+        TileButton(
+          key: const Key('plus_settings_button'),
+          leadingIconData: LineIcons.cog,
+          onPressed: () {},
+          title: 'SETTINGS',
+          trailingIconData: Icons.keyboard_arrow_right,
+        ),
+        TileButton(
+          key: const Key('plus_about_button'),
+          leadingIconData: Icons.info_outline_rounded,
+          onPressed: () {},
+          title: 'ABOUT',
+          trailingIconData: Icons.keyboard_arrow_right,
+        ),
+      ],
     );
   }
 }
--- lib/presentation/plus/plus_page.dart
+++ lib/presentation/plus/plus_page.dart
@@ -1,6 +1,10 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+
+import 'package:wine/presentation/core/buttons/asset_button.dart';
 import 'package:wine/presentation/plus/plus_layout.dart';
+import 'package:wine/utils/assets/icons.dart';
 import 'package:wine/utils/themes.dart';
 
 /// @nodoc
@@ -16,7 +20,42 @@ class PlusPage extends StatelessWidget {
     ]);
     return AnnotatedRegion<SystemUiOverlayStyle>(
       value: lightTheme,
-      child: const PlusLayout(),
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(41.5),
+          child: AppBar(
+            backgroundColor: Colors.transparent,
+            bottom: PreferredSize(
+              preferredSize: const Size.fromHeight(0.0),
+              child: Container(color: Colors.black, height: 2.0),
+            ),
+            brightness: Brightness.light,
+            centerTitle: true,
+            elevation: 0.0,
+            leading: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10.0,
+                vertical: 5.0,
+              ),
+              child: AssetButton(
+                key: const Key('plus_layout_back_button'),
+                imagePath: backIcon,
+                onPressed: () => context.router.pop(),
+              ),
+            ),
+            title: const Text(
+              'PLUS',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 18.0,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+          ),
+        ),
+        body: const PlusLayout(),
+      ),
     );
   }
 }
