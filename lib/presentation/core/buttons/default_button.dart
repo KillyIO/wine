33b2cdf595470a38cd8bda0a3ef5cb6dbd826b34
François Grèzes <grezesfrancois@gmail.com>
2021-06-25 23:49:08 +0200

    add default_button

---
+++ lib/presentation/core/buttons/default_button.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+/// @nodoc
+class DefaultButton extends StatelessWidget {
+  /// @nodoc
+  const DefaultButton({
+    Key? key,
+    required this.color,
+    this.fontSize,
+    this.hasRoundedCorners = false,
+    this.height,
+    required this.title,
+    this.onPressed,
+    this.width,
+  }) : super(key: key);
+
+  /// @nodoc
+  final Color color;
+
+  /// @nodoc
+  final double? fontSize;
+
+  /// @nodoc
+  final bool hasRoundedCorners;
+
+  /// @nodoc
+  final double? height;
+
+  /// @nodoc
+  final String title;
+
+  /// @nodoc
+  final VoidCallback? onPressed;
+
+  /// @nodoc
+  final double? width;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: height ?? 2.5.h,
+      width: width ?? 100.w,
+      child: TextButton(
+        onPressed: onPressed,
+        style: TextButton.styleFrom(
+          backgroundColor: color,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(
+              hasRoundedCorners ? 10 : 0.0,
+            ),
+          ),
+        ),
+        child: Text(
+          title,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: fontSize ?? 7.5.sp,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+    );
+  }
+}
