db56eae2bbaf19d1fe1fe11b2ca4a418e2480379
François Grèzes <grezesfrancois@gmail.com>
2021-07-07 21:22:15 +0200

    add basic library page

---
+++ lib/presentation/library/library_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+/// @nodoc
+class LibraryPage extends StatelessWidget {
+  /// @nodoc
+  const LibraryPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
