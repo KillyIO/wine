043fd1732207f59f107ae9b36a6cc55d207501d4
François Grèzes <grezesfrancois@gmail.com>
2021-07-10 23:22:17 +0200

    implement signup page

--- lib/presentation/sign_up/sign_up_page.dart
+++ lib/presentation/sign_up/sign_up_page.dart
@@ -1,4 +1,15 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sizer/sizer.dart';
+import 'package:wine/application/sign_up/sign_up_bloc.dart';
+import 'package:wine/injection.dart';
+
+import 'package:wine/presentation/core/buttons/asset_button.dart';
+import 'package:wine/presentation/sign_up/sign_up_layout.dart';
+import 'package:wine/utils/assets/icons.dart';
+import 'package:wine/utils/themes.dart';
 
 /// @nodoc
 class SignUpPage extends StatelessWidget {
@@ -7,6 +18,45 @@ class SignUpPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: lightTheme,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(41.5),
+          child: AppBar(
+            backgroundColor: Colors.transparent,
+            bottom: PreferredSize(
+              preferredSize: const Size.fromHeight(0.0),
+              child: Container(
+                color: Colors.black,
+                height: 0.15.h,
+              ),
+            ),
+            brightness: Brightness.light,
+            centerTitle: true,
+            elevation: 0.0,
+            leading: Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 1.w,
+                vertical: .5.h,
+              ),
+              child: AssetButton(
+                key: const Key('plus_layout_back_button'),
+                imagePath: backIcon,
+                onPressed: () {
+                  FocusScope.of(context).requestFocus(FocusNode());
+                  context.router.root.pop();
+                },
+              ),
+            ),
+          ),
+        ),
+        body: BlocProvider(
+          create: (_) => getIt<SignUpBloc>(),
+          child: const SignUpLayout(),
+        ),
+      ),
+    );
   }
 }
