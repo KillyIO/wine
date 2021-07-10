8e2fe728282d886aee2acb4fc6e08d897764c7bc
François Grèzes <grezesfrancois@gmail.com>
2021-07-10 23:22:01 +0200

    add custom label for signup form

---
+++ lib/presentation/core/labels/text_field_label.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+/// @nodoc
+class TextFieldLabel extends StatelessWidget {
+  /// @nodoc
+  const TextFieldLabel({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  /// @nodoc
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      width: MediaQuery.of(context).size.width,
+      height: 50.0,
+      child: Align(
+        alignment: Alignment.bottomLeft,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20.0, bottom: 10.0),
+          child: Text(
+            title,
+            style: const TextStyle(
+              color: Colors.black,
+              fontSize: 13.0,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
