2f314d3a6823680724eb5f6c52d6ade812307d43
François Grèzes <grezesfrancois@gmail.com>
2021-07-13 19:03:49 +0200

    add SuccessDialog

--- lib/presentation/core/dialogs/success_dialog.dart
+++ lib/presentation/core/dialogs/success_dialog.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:line_icons/line_icons.dart';
+import 'package:sizer/sizer.dart';
+import 'package:wine/utils/constants/palette.dart';
 
 /// @nodoc
 class SuccessDialog extends StatelessWidget {
@@ -23,7 +26,65 @@ class SuccessDialog extends StatelessWidget {
   Widget build(BuildContext context) {
     return Dialog(
       elevation: 10.0,
-      child: Container(),
+      child: Container(
+        color: Colors.white,
+        width: 50.w,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 1.25.h),
+              child: Icon(
+                LineIcons.checkCircle,
+                color: success,
+                size: 25.sp,
+              ),
+            ),
+            Text(
+              'SUCCESS!',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 7.5.sp,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                bottom: 2.h,
+                left: 2.w,
+                right: 2.w,
+                top: 1.h,
+              ),
+              child: Text(
+                message,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 7.sp,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            ),
+            Container(
+              color: success,
+              width: double.infinity,
+              child: TextButton(
+                onPressed: onPressed,
+                child: Text(
+                  buttonText ?? 'CONTINUE',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 8.5.sp,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
