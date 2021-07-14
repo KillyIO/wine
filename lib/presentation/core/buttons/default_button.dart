b0476f5353630364c6ba76a0d74db14c31d55b77
François Grèzes <grezesfrancois@gmail.com>
2021-07-14 14:19:45 +0200

    add progress indicator

--- lib/presentation/core/buttons/default_button.dart
+++ lib/presentation/core/buttons/default_button.dart
@@ -8,6 +8,7 @@ class DefaultButton extends StatelessWidget {
     Key? key,
     required this.color,
     this.fontSize,
+    this.isProcessing = false,
     this.hasRoundedCorners = false,
     this.height,
     required this.title,
@@ -20,6 +21,9 @@ class DefaultButton extends StatelessWidget {
 
   /// @nodoc
   final double? fontSize;
+
+  /// @nodoc
+  final bool isProcessing;
 
   /// @nodoc
   final bool hasRoundedCorners;
@@ -51,14 +55,22 @@ class DefaultButton extends StatelessWidget {
             ),
           ),
         ),
-        child: Text(
-          title,
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: fontSize ?? 7.5.sp,
-            fontWeight: FontWeight.w500,
-          ),
-        ),
+        child: isProcessing
+            ? SizedBox(
+                height: 2.h,
+                width: 2.h,
+                child: const CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                ),
+              )
+            : Text(
+                title,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: fontSize ?? 7.5.sp,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
       ),
     );
   }
