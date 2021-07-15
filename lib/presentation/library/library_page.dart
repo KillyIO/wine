25b46ae03387ee2be98f095cac36f61ae4be7542
François Grèzes <grezesfrancois@gmail.com>
2021-07-15 20:39:08 +0200

    add SystemChrome to LibraryPage

--- lib/presentation/library/library_page.dart
+++ lib/presentation/library/library_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 /// @nodoc
 class LibraryPage extends StatelessWidget {
@@ -7,6 +8,10 @@ class LibraryPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    return Scaffold();
   }
 }
