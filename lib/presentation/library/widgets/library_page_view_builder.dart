f194043d763b7918a1caca71990e0507f9a8407e
François Grèzes <grezesfrancois@gmail.com>
2021-08-05 22:37:36 +0200

    add library pageview builder (WIP)

---
+++ lib/presentation/library/widgets/library_page_view_builder.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:wine/application/library/library_navigation/library_navigation_bloc.dart';
+
+/// @nodoc
+class LibraryPageViewBuilder extends StatelessWidget {
+  /// @nodoc
+  LibraryPageViewBuilder({
+    Key? key,
+    required this.pageController,
+  }) : super(key: key);
+
+  /// @nodoc
+  final PageController pageController;
+
+  // TODO add real layouts
+  final List<Widget> _pageViewLayouts = <Widget>[Container(), Container()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: BlocBuilder<LibraryNavigationBloc, LibraryNavigationState>(
+        builder: (context, state) {
+          return PageView.builder(
+            controller: pageController,
+            itemBuilder: (context, index) =>
+                _pageViewLayouts[index % _pageViewLayouts.length],
+            onPageChanged: (index) {
+              context
+                  .read<LibraryNavigationBloc>()
+                  .add(LibraryNavigationEvent.pageViewIndexChanged(
+                    index % _pageViewLayouts.length,
+                  ));
+            },
+          );
+        },
+      ),
+    );
+  }
+}
