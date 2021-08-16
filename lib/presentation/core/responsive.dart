0c5890c04af769097e2908aecc87c3ed02bc05bf
François Grèzes <grezesfrancois@gmail.com>
2021-08-16 23:23:34 +0200

    add responsive constants + widget

---
+++ lib/presentation/core/responsive.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:wine/utils/constants/responsive.dart';
+
+/// @nodoc
+class Responsive extends StatelessWidget {
+  /// @nodoc
+  const Responsive({
+    Key? key,
+    this.mobileSmall,
+    required this.mobileNormal,
+    this.mobileLarge,
+    this.mobileExtraLarge,
+    this.tabletSmall,
+    this.tabletNormal,
+    this.tabletLarge,
+    this.tabletExtraLarge,
+    this.desktopSmall,
+    required this.desktopNormal,
+    this.desktopLarge,
+    this.desktopExtraLarge,
+  }) : super(key: key);
+
+  /// @nodoc
+  final Widget? mobileSmall;
+
+  /// @nodoc
+  final Widget mobileNormal;
+
+  /// @nodoc
+  final Widget? mobileLarge;
+
+  /// @nodoc
+  final Widget? mobileExtraLarge;
+
+  /// @nodoc
+  final Widget? tabletSmall;
+
+  /// @nodoc
+  final Widget? tabletNormal;
+
+  /// @nodoc
+  final Widget? tabletLarge;
+
+  /// @nodoc
+  final Widget? tabletExtraLarge;
+
+  /// @nodoc
+  final Widget? desktopSmall;
+
+  /// @nodoc
+  final Widget desktopNormal;
+
+  /// @nodoc
+  final Widget? desktopLarge;
+
+  /// @nodoc
+  final Widget? desktopExtraLarge;
+
+  @override
+  Widget build(BuildContext context) {
+    if (isMobileSmall(context) && mobileSmall != null) {
+      return mobileSmall!;
+    } else if (isMobileLarge(context) && mobileLarge != null) {
+      return mobileLarge!;
+    } else if (isMobileExtraLarge(context) && mobileExtraLarge != null) {
+      return mobileExtraLarge!;
+    } else if (isTabletSmall(context) && tabletSmall != null) {
+      return tabletSmall!;
+    } else if (isTabletNormal(context) && tabletNormal != null) {
+      return tabletNormal!;
+    } else if (isTabletLarge(context) && tabletLarge != null) {
+      return tabletLarge!;
+    } else if (isTabletExtraLarge(context) && tabletExtraLarge != null) {
+      return tabletExtraLarge!;
+    } else if (isDesktopSmall(context) && desktopSmall != null) {
+      return desktopSmall!;
+    } else if (isDesktopNormal(context)) {
+      return desktopNormal;
+    } else if (isDesktopLarge(context) && desktopLarge != null) {
+      return desktopLarge!;
+    } else if (isDesktopExtraLarge(context) && desktopExtraLarge != null) {
+      return desktopExtraLarge!;
+    }
+    return mobileNormal;
+  }
+}
---
+++ lib/utils/constants/responsive.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+/// @nodoc
+const double mobileSmall = 320;
+
+/// @nodoc
+const double mobileNormal = 375;
+
+/// @nodoc
+const double mobileLarge = 414;
+
+/// @nodoc
+const double mobileExtraLarge = 480;
+
+/// @nodoc
+const double tabletSmall = 600;
+
+/// @nodoc
+const double tabletNormal = 768;
+
+/// @nodoc
+const double tabletLarge = 850;
+
+/// @nodoc
+const double tabletExtraLarge = 900;
+
+/// @nodoc
+const double desktopSmall = 950;
+
+/// @nodoc
+const double desktopNormal = 1920;
+
+/// @nodoc
+const double desktopLarge = 3840;
+
+/// @nodoc
+const double desktopExtraLarge = 4096;
+
+/// @nodoc
+bool isMobileSmall(BuildContext context) =>
+    MediaQuery.of(context).size.width <= mobileSmall;
+
+/// @nodoc
+bool isMobileNormal(BuildContext context) =>
+    MediaQuery.of(context).size.width > mobileSmall &&
+    MediaQuery.of(context).size.width <= mobileNormal;
+
+/// @nodoc
+bool isMobileLarge(BuildContext context) =>
+    MediaQuery.of(context).size.width > mobileNormal &&
+    MediaQuery.of(context).size.width <= mobileLarge;
+
+/// @nodoc
+bool isMobileExtraLarge(BuildContext context) =>
+    MediaQuery.of(context).size.width > mobileLarge &&
+    MediaQuery.of(context).size.width <= mobileExtraLarge;
+
+/// @nodoc
+bool isTabletSmall(BuildContext context) =>
+    MediaQuery.of(context).size.width > mobileExtraLarge &&
+    MediaQuery.of(context).size.width <= tabletSmall;
+
+/// @nodoc
+bool isTabletNormal(BuildContext context) =>
+    MediaQuery.of(context).size.width > tabletSmall &&
+    MediaQuery.of(context).size.width <= tabletNormal;
+
+/// @nodoc
+bool isTabletLarge(BuildContext context) =>
+    MediaQuery.of(context).size.width > tabletNormal &&
+    MediaQuery.of(context).size.width <= tabletLarge;
+
+/// @nodoc
+bool isTabletExtraLarge(BuildContext context) =>
+    MediaQuery.of(context).size.width > tabletLarge &&
+    MediaQuery.of(context).size.width <= tabletExtraLarge;
+
+/// @nodoc
+bool isDesktopSmall(BuildContext context) =>
+    MediaQuery.of(context).size.width > tabletExtraLarge &&
+    MediaQuery.of(context).size.width <= desktopSmall;
+
+/// @nodoc
+bool isDesktopNormal(BuildContext context) =>
+    MediaQuery.of(context).size.width > desktopSmall &&
+    MediaQuery.of(context).size.width <= desktopNormal;
+
+/// @nodoc
+bool isDesktopLarge(BuildContext context) =>
+    MediaQuery.of(context).size.width > desktopNormal &&
+    MediaQuery.of(context).size.width <= desktopLarge;
+
+/// @nodoc
+bool isDesktopExtraLarge(BuildContext context) =>
+    MediaQuery.of(context).size.width > desktopLarge &&
+    MediaQuery.of(context).size.width <= desktopExtraLarge;
