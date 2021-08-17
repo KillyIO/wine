ab304b7c955837e1fcd76e13f0c80e4f33197448
François Grèzes <grezesfrancois@gmail.com>
2021-08-17 22:28:44 +0200

    add max content and settings layout sizes

--- lib/utils/constants/core.dart
+++ lib/utils/constants/core.dart
@@ -6,3 +6,9 @@ const double defaultAppBarHeight = 41.5;
 
 /// @nodoc
 const double defaultToolbarItemWidth = 41.5;
+
+/// @nodoc
+const double maxContentLayoutWidth = 1440;
+
+/// @nodoc
+const double maxSettingsLayoutWidth = 1080;
