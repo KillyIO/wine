e223c32b30addce6065ab18d43b37b1d53ecafd0
François Grèzes <grezesfrancois@gmail.com>
2021-08-19 23:11:47 +0200

    implement basic filters menu layout (WIP)

--- lib/presentation/home/home_filters_layout.dart
+++ lib/presentation/home/home_filters_layout.dart
@@ -1,4 +1,9 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:line_icons/line_icons.dart';
+
+import 'package:wine/utils/constants/core.dart';
+import 'package:wine/utils/responsive/drawer_responsive.dart';
 
 /// @nodoc
 class HomeFiltersLayout extends StatelessWidget {
@@ -7,6 +12,44 @@ class HomeFiltersLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final mediaQuery = MediaQuery.of(context).size;
+
+    return SizedBox(
+      width: getDrawerWidth(mediaQuery),
+      child: Drawer(
+        key: const Key('home_filters_drawer'),
+        child: Scaffold(
+          appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(defaultAppBarHeight),
+            child: AppBar(
+              backgroundColor: Colors.transparent,
+              brightness: Brightness.light,
+              elevation: 0,
+              leading: Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Builder(
+                  builder: (context) {
+                    return IconButton(
+                      highlightColor: Colors.transparent,
+                      hoverColor: Colors.transparent,
+                      icon: const Icon(
+                        LineIcons.times,
+                        color: Colors.black,
+                      ),
+                      onPressed: context.router.pop,
+                      splashColor: Colors.transparent,
+                    );
+                  },
+                ),
+              ),
+              leadingWidth: defaultToolbarItemWidth,
+            ),
+          ),
+          body: SafeArea(
+            child: Container(),
+          ),
+        ),
+      ),
+    );
   }
 }
