83d1d329d339f8baeade395b102b306583757f88
François Grèzes <grezesfrancois@gmail.com>
2021-08-19 21:23:04 +0200

    replace switch case for if else

--- lib/utils/responsive/dialog_responsive.dart
+++ lib/utils/responsive/dialog_responsive.dart
@@ -5,94 +5,80 @@ import 'package:responsive_builder/responsive_builder.dart';
 double getDialogButtonFontSize(Size size) {
   final deviceType = getDeviceType(size);
 
-  switch (deviceType) {
-    case DeviceScreenType.mobile:
-      return 20;
-    default:
-      return 22.5;
+  if (deviceType == DeviceScreenType.mobile) {
+    return 20;
   }
+  return 22.5;
 }
 
 /// @nodoc#
 double getDialogButtonHeight(Size size) {
   final deviceType = getDeviceType(size);
 
-  switch (deviceType) {
-    case DeviceScreenType.desktop:
-      return 50;
-    default:
-      return 40;
+  if (deviceType == DeviceScreenType.mobile) {
+    return 50;
   }
+  return 40;
 }
 
 /// @nodoc#
 double getDialogIconSize(Size size) {
   final deviceType = getDeviceType(size);
 
-  switch (deviceType) {
-    case DeviceScreenType.mobile:
-      return 60;
-    default:
-      return 75;
+  if (deviceType == DeviceScreenType.mobile) {
+    return 60;
   }
+  return 75;
 }
 
 /// @nodoc#
 EdgeInsets getDialogIconPadding(Size size) {
   final deviceType = getDeviceType(size);
 
-  switch (deviceType) {
-    case DeviceScreenType.mobile:
-      return const EdgeInsets.symmetric(vertical: 10);
-    default:
-      return const EdgeInsets.symmetric(vertical: 15);
+  if (deviceType == DeviceScreenType.mobile) {
+    return const EdgeInsets.symmetric(vertical: 10);
   }
+  return const EdgeInsets.symmetric(vertical: 15);
 }
 
 /// @nodoc
 double getDialogMessageFontSize(Size size) {
   final deviceType = getDeviceType(size);
 
-  switch (deviceType) {
-    case DeviceScreenType.mobile:
-      return 16;
-    default:
-      return 18.5;
+  if (deviceType == DeviceScreenType.mobile) {
+    return 16;
   }
+  return 18.5;
 }
 
 /// @nodoc
 EdgeInsets getDialogMessagePadding(Size size) {
   final deviceType = getDeviceType(size);
 
-  switch (deviceType) {
-    case DeviceScreenType.mobile:
-      return const EdgeInsets.only(
-        bottom: 18,
-        left: 16,
-        right: 16,
-        top: 8,
-      );
-    default:
-      return const EdgeInsets.only(
-        bottom: 22.5,
-        left: 22.5,
-        right: 22.5,
-        top: 12,
-      );
+  if (deviceType == DeviceScreenType.mobile) {
+    return const EdgeInsets.only(
+      bottom: 18,
+      left: 16,
+      right: 16,
+      top: 8,
+    );
   }
+  return const EdgeInsets.only(
+    bottom: 22.5,
+    left: 22.5,
+    right: 22.5,
+    top: 12,
+  );
 }
 
 /// @nodoc
 double getDialogTitleFontSize(Size size) {
   final deviceType = getDeviceType(size);
 
-  switch (deviceType) {
-    case DeviceScreenType.mobile:
-      return 18;
-    default:
-      return 20;
+  if (deviceType == DeviceScreenType.mobile) {
+    return 18;
   }
+  return 20;
 }
 
 /// @nodoc
