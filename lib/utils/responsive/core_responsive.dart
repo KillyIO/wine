5ab3b52c0360caa08f81020739ccfcc98156f3df
François Grèzes <grezesfrancois@gmail.com>
2021-08-21 23:00:40 +0200

    add responsive methods for horizontal navbar + tile button

--- lib/utils/responsive/core_responsive.dart
+++ lib/utils/responsive/core_responsive.dart
@@ -10,3 +10,45 @@ EdgeInsets getAssetBackButtonPadding(Size size) {
   }
   return const EdgeInsets.only(left: 10);
 }
+
+/// @nodoc
+double? getHorizontalPageViewNavbarButtonFontSize(Size size) {
+  final deviceType = getDeviceType(size);
+
+  if (deviceType == DeviceScreenType.desktop) {
+    return 16;
+  }
+}
+
+/// @nodoc
+double getHorizontalPageViewNavbarHeight(Size size) {
+  final deviceType = getDeviceType(size);
+
+  if (deviceType == DeviceScreenType.desktop) {
+    return 40;
+  }
+  return 30;
+}
+
+/// @nodoc
+Border getTileButtonBorder(Size size, {bool isFirst = false}) {
+  final deviceType = getDeviceType(size);
+
+  if (deviceType == DeviceScreenType.desktop) {
+    return isFirst
+        ? Border.all()
+        : const Border(
+            bottom: BorderSide(),
+            left: BorderSide(),
+            right: BorderSide(),
+          );
+  }
+  return isFirst
+      ? const Border(
+          bottom: BorderSide(),
+          top: BorderSide(),
+        )
+      : const Border(
+          bottom: BorderSide(),
+        );
+}
