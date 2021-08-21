b53aa9ab8873e8bf25087c7777ba32abe59f8060
François Grèzes <grezesfrancois@gmail.com>
2021-08-21 23:09:59 +0200

    move auth guarded navigation handler to new file

--- lib/presentation/home/home_menu_layout.dart
+++ lib/presentation/home/home_menu_layout.dart
@@ -1,18 +1,11 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:responsive_builder/responsive_builder.dart';
 
-import 'package:wine/application/auth/auth_bloc.dart';
-import 'package:wine/application/auth/auth_dialog/auth_dialog_cubit.dart';
-import 'package:wine/application/log_in/log_in_bloc.dart';
-import 'package:wine/application/sign_up/sign_up_bloc.dart';
-import 'package:wine/injection.dart';
 import 'package:wine/presentation/home/widgets/home_menu_tile.dart';
 import 'package:wine/presentation/routes/router.dart';
-import 'package:wine/presentation/web/auth_dialog.dart';
 import 'package:wine/utils/constants/core.dart';
+import 'package:wine/utils/functions/navigation_functions.dart';
 import 'package:wine/utils/responsive/drawer_responsive.dart';
 import 'package:wine/utils/responsive/home_responsive.dart';
 
@@ -20,41 +13,6 @@ import 'package:wine/utils/responsive/home_responsive.dart';
 class HomeMenuLayout extends StatelessWidget {
   /// @nodoc
   const HomeMenuLayout({Key? key}) : super(key: key);
-
-  Future<void> _handleLibraryButtonPressed(BuildContext context) async {
-    final mediaQuery = MediaQuery.of(context).size;
-    final deviceType = getDeviceType(mediaQuery);
-
-    if (deviceType != DeviceScreenType.mobile) {
-      final state = context.read<AuthBloc>().state;
-
-      await state.maybeMap(
-        authenticated: (_) => context.router.root.push(const LibraryRoute()),
-        orElse: () => showDialog<bool>(
-          context: context,
-          barrierDismissible: false,
-          builder: (_) => MultiBlocProvider(
-            providers: [
-              BlocProvider(
-                create: (_) => getIt<AuthDialogCubit>(),
-              ),
-              BlocProvider(
-                create: (context) => getIt<LogInBloc>(),
-              ),
-              BlocProvider(
-                create: (context) => getIt<SignUpBloc>(),
-              ),
-            ],
-            child: const AuthDialog(
-              key: Key('auth_dialog'),
-            ),
-          ),
-        ),
-      );
-    } else {
-      await context.router.root.push(const LibraryRoute());
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -101,7 +59,10 @@ class HomeMenuLayout extends StatelessWidget {
                   Expanded(child: Container()),
                   HomeMenuTile(
                     key: const Key('home_menu_library_tile'),
-                    onPressed: () => _handleLibraryButtonPressed(context),
+                    onPressed: () => handleAuthGuardedNavigation(
+                      context,
+                      const LibraryRoute(),
+                    ),
                     text: 'LIBRARY',
                   ),
                   // BlocBuilder<AuthBloc, AuthState>(
