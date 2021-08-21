6c1eaebd878fcbd7ed6175c45d1b8a35d00d97c3
François Grèzes <grezesfrancois@gmail.com>
2021-08-21 23:10:50 +0200

    add width constraint + remove Builder

--- lib/presentation/settings/settings_layout.dart
+++ lib/presentation/settings/settings_layout.dart
@@ -1,10 +1,10 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:line_icons/line_icons.dart';
-import 'package:wine/application/auth/auth_bloc.dart';
+
 import 'package:wine/presentation/core/buttons/tile_button.dart';
 import 'package:wine/presentation/routes/router.dart';
+import 'package:wine/utils/constants/core.dart';
 
 /// @nodoc
 class SettingsLayout extends StatelessWidget {
@@ -14,37 +14,39 @@ class SettingsLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: ListView(
-        children: <Widget>[
-          BlocBuilder<AuthBloc, AuthState>(
-            builder: (context, state) {
-              return TileButton(
+      child: Center(
+        child: Container(
+          constraints: const BoxConstraints(
+            maxWidth: maxContentLayoutWidth2,
+          ),
+          child: ListView(
+            children: <Widget>[
+              TileButton(
                 leadingIconData: Icons.account_circle_outlined,
                 title: 'ACCOUNT',
                 trailingIconData: Icons.keyboard_arrow_right,
-                onPressed: () => state.maybeMap(
-                  authenticated: (_) =>
-                      context.router.push(const AccountSettingsRoute()),
-                  orElse: () => context.router.root.push(const LogInRoute()),
-                ),
-              );
-            },
+                onPressed: () =>
+                    context.router.push(const AccountSettingsRoute()),
+              ),
+              TileButton(
+                leadingIconData: LineIcons.book,
+                title: 'SERIES',
+                trailingIconData: Icons.keyboard_arrow_right,
+                // TODO add push series settings page
+                onPressed: () =>
+                    context.router.push(const SeriesSettingsRoute()),
+              ),
+              TileButton(
+                leadingIconData: LineIcons.bookOpen,
+                title: 'CHAPTER',
+                trailingIconData: Icons.keyboard_arrow_right,
+                // TODO add push chapter settings page
+                onPressed: () =>
+                    context.router.push(const ChapterSettingsRoute()),
+              )
+            ],
           ),
-          TileButton(
-            leadingIconData: LineIcons.book,
-            title: 'SERIES',
-            trailingIconData: Icons.keyboard_arrow_right,
-            // TODO add push series settings page
-            onPressed: () => context.router.push(const SeriesSettingsRoute()),
-          ),
-          TileButton(
-            leadingIconData: LineIcons.bookOpen,
-            title: 'CHAPTER',
-            trailingIconData: Icons.keyboard_arrow_right,
-            // TODO add push chapter settings page
-            onPressed: () => context.router.push(const ChapterSettingsRoute()),
-          )
-        ],
+        ),
       ),
     );
   }
