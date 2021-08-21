21b8abb544eac29069aef9ba30fa7e52e16273ec
François Grèzes <grezesfrancois@gmail.com>
2021-08-21 16:01:37 +0200

    update padding "top" param

--- lib/utils/responsive/log_in_responsive.dart
+++ lib/utils/responsive/log_in_responsive.dart
@@ -16,7 +16,7 @@ EdgeInsets getWelcomeMessagePadding(Size size) {
   final deviceType = getDeviceType(size);
 
   if (deviceType == DeviceScreenType.mobile) {
-    return const EdgeInsets.only(left: 50, top: 100);
+    return const EdgeInsets.only(left: 50, top: 75);
   }
   return const EdgeInsets.only(left: 50, top: 50);
 }
