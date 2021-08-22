a232adc13346d749897368473b22a0415f5924b6
François Grèzes <grezesfrancois@gmail.com>
2021-08-22 23:12:44 +0200

    remove AuthBloc BlocBuilder

--- lib/presentation/settings/account_settings/account_settings_layout.dart
+++ lib/presentation/settings/account_settings/account_settings_layout.dart
@@ -1,8 +1,8 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import 'package:wine/application/auth/auth_bloc.dart';
+
 import 'package:wine/application/settings/settings_bloc.dart';
 import 'package:wine/presentation/core/misc/section_tile.dart';
 import 'package:wine/presentation/routes/router.dart';
@@ -23,85 +23,56 @@ class AccountSettingsLayout extends StatelessWidget {
             ..router.root.navigate(const HomeRoute());
         }
       },
-      child: BlocBuilder<AuthBloc, AuthState>(
-        builder: (context, authState) {
-          return BlocBuilder<SettingsBloc, SettingsState>(
-            builder: (context, settingsState) {
-              return Column(
-                children: <Widget>[
-                  const SectionTile(title: 'ACCOUNT'),
-                  authState.maybeMap(
-                    authenticated: (_) => ListTile(
-                      title: const Text(
-                        'USERNAME',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 15,
-                          fontWeight: FontWeight.w300,
-                        ),
-                      ),
-                      trailing: Text(
-                        '@${settingsState.username}',
-                        overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(
-                          color: Colors.black,
-                          fontSize: 15,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ),
-                    orElse: () => Container(),
+      child: BlocBuilder<SettingsBloc, SettingsState>(
+        builder: (context, state) {
+          return Column(
+            children: <Widget>[
+              const SectionTile(title: 'ACCOUNT'),
+              ListTile(
+                title: const Text(
+                  'USERNAME',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w300,
                   ),
-                  authState.maybeMap(
-                    authenticated: (_) => Container(
-                      color: error,
-                      child: ListTile(
-                        onTap: () => context
-                            .read<SettingsBloc>()
-                            .add(const SettingsEvent.logOutPressed()),
-                        leading: const Icon(
-                          Icons.exit_to_app,
-                          color: Colors.white,
-                        ),
-                        title: const Text(
-                          'LOG OUT',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 15,
-                            fontWeight: FontWeight.w500,
-                            letterSpacing: 0.5,
-                          ),
-                        ),
-                        trailing: const Icon(
-                          Icons.keyboard_arrow_right,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                    orElse: () => ListTile(
-                      onTap: () => context.router.root.push(const LogInRoute()),
-                      title: const Text(
-                        'MY ACCOUNT',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 15,
-                          fontWeight: FontWeight.w300,
-                        ),
-                      ),
-                      trailing: const Text(
-                        'PLEASE LOG IN',
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 15,
-                          fontWeight: FontWeight.w200,
-                        ),
-                      ),
+                ),
+                trailing: Text(
+                  '@${state.username}',
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+              Container(
+                color: error,
+                child: ListTile(
+                  onTap: () => context
+                      .read<SettingsBloc>()
+                      .add(const SettingsEvent.logOutPressed()),
+                  leading: const Icon(
+                    Icons.exit_to_app,
+                    color: Colors.white,
+                  ),
+                  title: const Text(
+                    'LOG OUT',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                      fontWeight: FontWeight.w500,
+                      letterSpacing: 0.5,
                     ),
                   ),
-                ],
-              );
-            },
+                  trailing: const Icon(
+                    Icons.keyboard_arrow_right,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
           );
         },
       ),
