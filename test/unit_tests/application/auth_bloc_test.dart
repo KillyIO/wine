1d4ab1b7a732eff9c6826130dbbc1d78b8f58f84
François Grèzes <grezesfrancois@gmail.com>
2021-09-05 01:52:58 +0200

    fix tests

--- test/unit_tests/application/auth_bloc_test.dart
+++ test/unit_tests/application/auth_bloc_test.dart
@@ -1,18 +1,26 @@
 import 'package:bloc_test/bloc_test.dart';
+import 'package:firebase_auth/firebase_auth.dart' as auth;
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:rustic/option.dart';
 import 'package:wine/application/auth/auth_bloc.dart';
 import 'package:wine/domain/auth/i_auth_facade.dart';
 
 import '../../mocks/auth_facade_mocks.dart';
+import '../../mocks/firebase_auth_mocks.dart';
+import '../utils/constants.dart';
 
 void main() {
   late IAuthFacade _authFacade;
+
+  late auth.User _firebaseUser;
 
   late AuthBloc _authBloc;
 
   setUp(() {
     _authFacade = MockAuthFacade();
+
+    _firebaseUser = MockUser();
 
     _authBloc = AuthBloc(_authFacade);
   });
@@ -23,38 +31,16 @@ void main() {
 
   group('AuthBloc -', () {
     blocTest<AuthBloc, AuthState>(
-      'emits [] when instanciated.',
-      build: () => _authBloc,
-      expect: () => <AuthState>[],
-    );
-
-    blocTest<AuthBloc, AuthState>(
       'emits [AuthState.anonymous] when authChanged is added 1.',
       build: () => _authBloc,
       act: (bloc) {
-        when(() => _authFacade.isLoggedIn).thenReturn(true);
-        when(() => _authFacade.isAnonymous).thenReturn(true);
+        when(() => _authFacade.authStateChanges)
+            .thenAnswer((_) => Stream.fromIterable([Option(null)]));
         return bloc.add(const AuthEvent.authChanged());
       },
       expect: () => <AuthState>[const AuthState.anonymous()],
       verify: (_) {
-        verify(() => _authFacade.isLoggedIn).called(1);
-        verify(() => _authFacade.isAnonymous).called(1);
-      },
-    );
-
-    blocTest<AuthBloc, AuthState>(
-      'emits [AuthState.anonymous] when authChanged is added 2.',
-      build: () => _authBloc,
-      act: (bloc) {
-        when(() => _authFacade.isLoggedIn).thenReturn(false);
-        when(() => _authFacade.isAnonymous).thenReturn(false);
-        return bloc.add(const AuthEvent.authChanged());
-      },
-      expect: () => <AuthState>[const AuthState.anonymous()],
-      verify: (_) {
-        verify(() => _authFacade.isLoggedIn).called(1);
-        verifyNever(() => _authFacade.isAnonymous);
+        verify(() => _authFacade.authStateChanges).called(1);
       },
     );
 
@@ -62,14 +48,13 @@ void main() {
       'emits [AuthState.authenticated] when authChanged is added.',
       build: () => _authBloc,
       act: (bloc) {
-        when(() => _authFacade.isLoggedIn).thenReturn(true);
-        when(() => _authFacade.isAnonymous).thenReturn(false);
+        when(() => _authFacade.authStateChanges)
+            .thenAnswer((_) => Stream.fromIterable([Option(testUser)]));
         return bloc.add(const AuthEvent.authChanged());
       },
       expect: () => <AuthState>[const AuthState.authenticated()],
       verify: (_) {
-        verify(() => _authFacade.isLoggedIn).called(1);
-        verify(() => _authFacade.isAnonymous).called(1);
+        verify(() => _authFacade.authStateChanges).called(1);
       },
     );
   });
