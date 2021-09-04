c1d0c5b1fab9dd372c5db74489ed6765be3e3eec
François Grèzes <grezesfrancois@gmail.com>
2021-09-05 01:56:10 +0200

    remove unused variables

--- test/unit_tests/application/auth_bloc_test.dart
+++ test/unit_tests/application/auth_bloc_test.dart
@@ -1,5 +1,4 @@
 import 'package:bloc_test/bloc_test.dart';
-import 'package:firebase_auth/firebase_auth.dart' as auth;
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:rustic/option.dart';
@@ -7,20 +6,15 @@ import 'package:wine/application/auth/auth_bloc.dart';
 import 'package:wine/domain/auth/i_auth_facade.dart';
 
 import '../../mocks/auth_facade_mocks.dart';
-import '../../mocks/firebase_auth_mocks.dart';
 import '../utils/constants.dart';
 
 void main() {
   late IAuthFacade _authFacade;
 
-  late auth.User _firebaseUser;
-
   late AuthBloc _authBloc;
 
   setUp(() {
     _authFacade = MockAuthFacade();
-
-    _firebaseUser = MockUser();
 
     _authBloc = AuthBloc(_authFacade);
   });
