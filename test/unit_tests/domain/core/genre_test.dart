1063cec28f3e5b04c397d1e4b07a3e88f1b06edb
François Grèzes <grezesfrancois@gmail.com>
2021-09-10 18:26:28 +0200

    fix Series + Genre tests

--- test/unit_tests/domain/core/genre_test.dart
+++ test/unit_tests/domain/core/genre_test.dart
@@ -13,21 +13,6 @@ void main() {
       expect(genre.value, const Ok<String, ValueFailure<String>>(testGenre));
     });
 
-    test('When input empty And is optional Then return input', () {
-      final genre = Genre(testEmpty, isOptional: true);
-
-      expect(genre.value, const Ok<String, ValueFailure<String>>(testEmpty));
-    });
-
-    test('When input valid And is optional Then return input', () {
-      final genre = Genre(testGenreOptional, isOptional: true);
-
-      expect(
-        genre.value,
-        const Ok<String, ValueFailure<String>>(testGenreOptional),
-      );
-    });
-
     test(
       'When input empty And is not optional Then return emptySelection',
       () {
