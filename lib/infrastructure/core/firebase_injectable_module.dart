a8077580c11484abcbabef790efe1357beee2a4e
SSebigo <grezesfrancois@gmail.com>
2021-11-11 23:54:15 +0100

    add firebaseStorage dependency

--- lib/infrastructure/core/firebase_injectable_module.dart
+++ lib/infrastructure/core/firebase_injectable_module.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 // import 'package:firebase_storage/firebase_storage.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:injectable/injectable.dart';
@@ -9,17 +10,17 @@ import 'package:injectable/injectable.dart';
 abstract class FirebaseInjectableModule {
   /// @nodoc
   @lazySingleton
-  GoogleSignIn get googleSignIn => GoogleSignIn();
+  FirebaseAuth get firebaseAuth => FirebaseAuth.instance;
 
   /// @nodoc
   @lazySingleton
-  FirebaseAuth get firebaseAuth => FirebaseAuth.instance;
+  FirebaseStorage get firebaseStorage => FirebaseStorage.instance;
 
   /// @nodoc
   @lazySingleton
   FirebaseFirestore get firestore => FirebaseFirestore.instance;
 
   /// @nodoc
-  // @lazySingleton
-  // FirebaseStorage get firebaseStorage => FirebaseStorage.instance;
+  @lazySingleton
+  GoogleSignIn get googleSignIn => GoogleSignIn();
 }
