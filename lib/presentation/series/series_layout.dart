7a4603234dffc44a9020282ca93830eb9ab4e0dd
SSebigo <grezesfrancois@gmail.com>
2021-12-13 20:18:20 +0100

    add SeriesPage/Layout (not implemented)

---
+++ lib/presentation/series/series_layout.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+/// @nodoc
+class SeriesLayout extends StatelessWidget {
+  /// @nodoc
+  const SeriesLayout({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/presentation/series/series_page.dart
+++ lib/presentation/series/series_page.dart
@@ -1,14 +1,23 @@
-import 'package:auto_route/annotations.dart';
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:wine/application/series/series_bloc.dart';
+import 'package:wine/domain/core/unique_id.dart';
 import 'package:wine/domain/series/series.dart';
+import 'package:wine/injection.dart';
+import 'package:wine/presentation/series/series_layout.dart';
+import 'package:wine/utils/constants/core.dart';
+import 'package:wine/utils/responsive/core_responsive.dart';
 
 /// @nodoc
 class SeriesPage extends StatelessWidget {
   /// @nodoc
   const SeriesPage({
     Key? key,
-    @pathParam required this.id,
-    @queryParam this.series,
+    @PathParam('id') required this.id,
+    this.series,
   }) : super(key: key);
 
   /// nodoc
@@ -19,6 +28,59 @@ class SeriesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final mediaQuery = MediaQuery.of(context).size;
+
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(defaultAppBarHeight),
+        child: AppBar(
+          backgroundColor: Colors.transparent,
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(0),
+            child: Container(
+              color: Colors.black,
+              height: 2,
+            ),
+          ),
+          centerTitle: true,
+          elevation: 0,
+          leading: !kIsWeb
+              ? Padding(
+                  padding: getAssetBackButtonPadding(mediaQuery),
+                  child: IconButton(
+                    key: const Key('typewriter_back'),
+                    highlightColor: Colors.transparent,
+                    hoverColor: Colors.transparent,
+                    icon: const Icon(
+                      Icons.keyboard_backspace_outlined,
+                      color: Colors.black,
+                    ),
+                    onPressed: () {
+                      FocusScope.of(context).requestFocus(FocusNode());
+                      context.router.root.pop();
+                    },
+                    splashColor: Colors.transparent,
+                  ),
+                )
+              : Container(),
+          leadingWidth: defaultToolbarItemWidth,
+        ),
+      ),
+      body: BlocProvider(
+        create: (_) => getIt<SeriesBloc>()
+          ..add(
+            SeriesEvent.launchWithID(
+              UniqueID.fromUniqueString(id),
+              series: series,
+            ),
+          ),
+        child: const SeriesLayout(),
+      ),
+    );
   }
 }
