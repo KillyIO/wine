010ed807fffb0f2f7aa4638f3a3939bf1f8e40f9
SSebigo <grezesfrancois@gmail.com>
2021-12-13 20:19:45 +0100

    remove _generateTiles param

--- lib/presentation/library/widgets/library_base_series_layout.dart
+++ lib/presentation/library/widgets/library_base_series_layout.dart
@@ -1,8 +1,14 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:wine/application/library/library_bloc.dart';
+import 'package:wine/domain/core/typewriter_type.dart';
 import 'package:wine/domain/series/series.dart';
 import 'package:wine/presentation/core/series/series_card.dart';
+import 'package:wine/presentation/routes/router.dart';
 import 'package:wine/utils/assets/images.dart';
+import 'package:wine/utils/functions/navigation_functions.dart';
 
 /// @nodoc
 class LibraryBaseSeriesLayout extends StatelessWidget {
@@ -10,10 +16,14 @@ class LibraryBaseSeriesLayout extends StatelessWidget {
   const LibraryBaseSeriesLayout({
     Key? key,
     required this.seriesList,
+    required this.type,
   }) : super(key: key);
 
   /// @nodoc
   final List<Series> seriesList;
+
+  /// @nodoc
+  final String type;
 
   List<StaggeredTile> get _generateStaggeredTiles {
     final staggeredTiles = <StaggeredTile>[];
@@ -32,7 +42,14 @@ class LibraryBaseSeriesLayout extends StatelessWidget {
       tiles.add(
         SeriesCard(
           coverURL: s.coverURL.getOrNull() ?? '',
-          onPressed: () {},
+          onPressed: () {
+            switch (type) {
+              case 'published':
+              case 'drafts':
+              case 'bookmarks':
+              default:
+            }
+          },
           title: s.title.getOrNull() ?? '',
           uid: s.uid.getOrCrash(),
         ),
@@ -46,40 +63,74 @@ class LibraryBaseSeriesLayout extends StatelessWidget {
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context).size;
 
-    if (seriesList.isEmpty) {
-      return Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Image.asset(
-              treeImage,
-              fit: BoxFit.contain,
-              width: mediaQuery.width * .35,
+    return BlocBuilder<LibraryBloc, LibraryState>(
+      buildWhen: (previous, current) =>
+          current.isProcessing != previous.isProcessing,
+      builder: (context, _) {
+        if (seriesList.isEmpty) {
+          return Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Image.asset(
+                  treeImage,
+                  fit: BoxFit.contain,
+                  width: mediaQuery.width * .35,
+                ),
+                const Text(
+                  'Nothing was found.',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
             ),
-            const Text(
-              'Nothing was found.',
-              style: TextStyle(
-                fontWeight: FontWeight.w600,
+          );
+        }
+        return ScrollConfiguration(
+          behavior: const ScrollBehavior(),
+          child: ListView(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child: StaggeredGridView.count(
+                  crossAxisCount: 2,
+                  staggeredTiles: _generateStaggeredTiles,
+                  crossAxisSpacing: 20,
+                  shrinkWrap: true,
+                  children: [
+                    for (final s in seriesList)
+                      SeriesCard(
+                        coverURL: s.coverURL.getOrNull() ?? '',
+                        onPressed: () {
+                          switch (type) {
+                            case 'published':
+                              // context.router.root.push(const SeriesRoute());
+                              break;
+                            case 'drafts':
+                              handleAuthGuardedNavigation(
+                                context,
+                                navigateTo: TypewriterSeriesId(
+                                  id: s.uid.getOrCrash(),
+                                  series: s,
+                                  type: TypewriterType.series,
+                                ),
+                              );
+                              break;
+                            case 'bookmarks':
+                            default:
+                          }
+                        },
+                        title: s.title.getOrNull() ?? '',
+                        uid: s.uid.getOrCrash(),
+                      )
+                  ],
+                ),
               ),
-            ),
-          ],
-        ),
-      );
-    }
-    return ScrollConfiguration(
-      behavior: const ScrollBehavior(),
-      child: ListView(children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 20),
-          child: StaggeredGridView.count(
-            crossAxisCount: 2,
-            staggeredTiles: _generateStaggeredTiles,
-            crossAxisSpacing: 20,
-            shrinkWrap: true,
-            children: _generateTiles,
+            ],
           ),
-        ),
-      ]),
+        );
+      },
     );
   }
 }
