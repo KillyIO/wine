c4ce497b5e2b2b8b72e9b91bc21f9011ce56b9b4
SSebigo <grezesfrancois@gmail.com>
2022-01-02 21:10:58 +0100

    remove width + height params

--- lib/presentation/core/series/series_card.dart
+++ lib/presentation/core/series/series_card.dart
@@ -13,12 +13,10 @@ class SeriesCard extends StatelessWidget {
     // required this.authorUsername,
     // this.authorUsernameFontSize = 10,
     required this.coverURL,
-    this.height = 100,
     required this.onPressed,
     required this.title,
     this.titleFontSize = 12,
     required this.uid,
-    this.width = 50,
   }) : super(key: key);
 
   // /// @nodoc
@@ -29,9 +27,6 @@ class SeriesCard extends StatelessWidget {
 
   /// @nodoc
   final String coverURL;
-
-  /// @nodoc
-  final double height;
 
   /// @nodoc
   final VoidCallback onPressed;
@@ -45,70 +40,63 @@ class SeriesCard extends StatelessWidget {
   /// @nodoc
   final String uid;
 
-  /// @nodoc
-  final double width;
-
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onPressed,
-      child: SizedBox(
-        height: height,
-        width: width,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(bottom: 10),
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  boxShadow: const <BoxShadow>[
-                    BoxShadow(
-                      color: Colors.black12,
-                      spreadRadius: 1,
-                      blurRadius: 5,
-                      offset: Offset(0, 3),
-                    ),
-                  ],
-                ),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(6),
-                  child: Hero(
-                    tag: uid,
-                    child: isURL(coverURL)
-                        ? CachedNetworkImage(
-                            fit: BoxFit.contain,
-                            imageUrl: coverURL,
-                          )
-                        : Image.file(File(coverURL)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(bottom: 10),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: const <BoxShadow>[
+                  BoxShadow(
+                    color: Colors.black12,
+                    spreadRadius: 1,
+                    blurRadius: 5,
+                    offset: Offset(0, 3),
                   ),
+                ],
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(6),
+                child: Hero(
+                  tag: uid,
+                  child: isURL(coverURL)
+                      ? CachedNetworkImage(
+                          fit: BoxFit.contain,
+                          imageUrl: coverURL,
+                        )
+                      : Image.file(File(coverURL)),
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 1),
-              child: TextOneLine(
-                title,
-                style: TextStyle(
-                  fontWeight: FontWeight.w600,
-                  fontSize: titleFontSize,
-                ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 1),
+            child: TextOneLine(
+              title,
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: titleFontSize,
               ),
             ),
-            // Padding(
-            //   padding: const EdgeInsets.symmetric(horizontal: 1),
-            //   child: TextOneLine(
-            //     'Some text',
-            //     style: TextStyle(
-            //       color: Colors.black38,
-            //       fontWeight: FontWeight.w400,
-            //       fontSize: 10,
-            //     ),
-            //   ),
-            // ),
-          ],
-        ),
+          ),
+          // Padding(
+          //   padding: const EdgeInsets.symmetric(horizontal: 1),
+          //   child: TextOneLine(
+          //     'Some text',
+          //     style: TextStyle(
+          //       color: Colors.black38,
+          //       fontWeight: FontWeight.w400,
+          //       fontSize: 10,
+          //     ),
+          //   ),
+          // ),
+        ],
       ),
     );
   }
