2746ac42a0cbe7eed8938468326f48d32d27c775
SSebigo <grezesfrancois@gmail.com>
2022-01-04 22:34:28 +0100

    remove series_details params

--- lib/presentation/series/series_layout.dart
+++ lib/presentation/series/series_layout.dart
@@ -10,32 +10,24 @@ class SeriesLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<SeriesBloc, SeriesState>(
+    return BlocListener<SeriesBloc, SeriesState>(
       listener: (context, state) {
         // TODO(SSebigo): implement listener
       },
-      builder: (context, state) {
-        return Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 20),
-                  child: SeriesDetails(
-                    authorUsername: state.author.username.getOrNull() ??
-                        state.session.username.getOrNull(),
-                    coverURL: state.series.coverURL.getOrNull(),
-                    seriesUID: state.series.uid.getOrCrash(),
-                    title: state.series.title.getOrNull(),
-                  ),
-                )
-              ],
-            ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: SeriesDetails(),
+              )
+            ],
           ),
-        );
-      },
+        ),
+      ),
     );
   }
 }
--- lib/presentation/series/widgets/series_details.dart
+++ lib/presentation/series/widgets/series_details.dart
@@ -1,30 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:like_button/like_button.dart';
+import 'package:wine/application/series/series_bloc.dart';
 import 'package:wine/presentation/series/widgets/series_author_username.dart';
 import 'package:wine/presentation/series/widgets/series_cover.dart';
 import 'package:wine/presentation/series/widgets/series_title.dart';
+import 'package:wine/utils/constants/palette.dart';
 
 /// @nodoc
 class SeriesDetails extends StatelessWidget {
   /// @nodoc
-  const SeriesDetails({
-    Key? key,
-    required this.authorUsername,
-    required this.coverURL,
-    required this.seriesUID,
-    required this.title,
-  }) : super(key: key);
-
-  /// @nodoc
-  final String? authorUsername;
-
-  /// @nodoc
-  final String? coverURL;
-
-  /// @nodoc
-  final String seriesUID;
-
-  /// @nodoc
-  final String? title;
+  const SeriesDetails({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -34,9 +20,13 @@ class SeriesDetails extends StatelessWidget {
           padding: const EdgeInsets.only(right: 20),
           child: ClipRRect(
             borderRadius: BorderRadius.circular(6),
-            child: SeriesCover(
-              coverURL: coverURL,
-              seriesUID: seriesUID,
+            child: BlocBuilder<SeriesBloc, SeriesState>(
+              builder: (context, state) {
+                return SeriesCover(
+                  coverURL: state.series.coverURL.getOrNull(),
+                  seriesUID: state.series.uid.getOrCrash(),
+                );
+              },
             ),
           ),
         ),
@@ -51,16 +41,95 @@ class SeriesDetails extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   mainAxisSize: MainAxisSize.min,
                   children: [
-                    SeriesTitle(title: title),
+                    BlocBuilder<SeriesBloc, SeriesState>(
+                      builder: (context, state) {
+                        return SeriesTitle(
+                          title: state.series.title.getOrNull(),
+                        );
+                      },
+                    ),
                     Padding(
                       padding: const EdgeInsets.only(top: 5.5),
-                      child:
-                          SeriesAuthorUsername(authorUsername: authorUsername),
+                      child: BlocBuilder<SeriesBloc, SeriesState>(
+                        builder: (context, state) {
+                          return SeriesAuthorUsername(
+                            authorUsername: state.author.username.getOrNull() ??
+                                state.session.username.getOrNull(),
+                          );
+                        },
+                      ),
                     ),
                   ],
                 ),
                 Row(
-                  children: [],
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 20),
+                      child: BlocBuilder<SeriesBloc, SeriesState>(
+                        builder: (context, state) {
+                          return LikeButton(
+                            likeBuilder: (_) {
+                              return const Icon(
+                                Icons.visibility,
+                                color: Colors.black38,
+                              );
+                            },
+                            likeCount: state.series.viewsCount,
+                          );
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 20),
+                      child: BlocBuilder<SeriesBloc, SeriesState>(
+                        builder: (context, state) {
+                          return LikeButton(
+                            bubblesColor: BubblesColor(
+                              dotPrimaryColor: pastelPink,
+                              dotSecondaryColor:
+                                  pastelPink.withGreen(122).withBlue(112),
+                            ),
+                            circleColor: CircleColor(
+                              start: pastelPink,
+                              end: pastelPink.withGreen(122).withBlue(112),
+                            ),
+                            isLiked: state.isLiked,
+                            likeBuilder: (isLiked) {
+                              return Icon(
+                                Icons.favorite,
+                                color: isLiked ? pastelPink : Colors.black38,
+                              );
+                            },
+                            likeCount: state.series.likesCount,
+                          );
+                        },
+                      ),
+                    ),
+                    BlocBuilder<SeriesBloc, SeriesState>(
+                      builder: (context, state) {
+                        return LikeButton(
+                          bubblesColor: BubblesColor(
+                            dotPrimaryColor: pastelYellow,
+                            dotSecondaryColor:
+                                pastelYellow.withGreen(214).withBlue(69),
+                          ),
+                          circleColor: CircleColor(
+                            start: pastelYellow,
+                            end: pastelYellow.withGreen(214).withBlue(69),
+                          ),
+                          isLiked: state.isBookmarked,
+                          likeBuilder: (isBookmarked) {
+                            return Icon(
+                              Icons.bookmark,
+                              color:
+                                  isBookmarked ? pastelYellow : Colors.black38,
+                            );
+                          },
+                          likeCount: state.series.bookmarksCount,
+                        );
+                      },
+                    ),
+                  ],
                 ),
               ],
             ),
