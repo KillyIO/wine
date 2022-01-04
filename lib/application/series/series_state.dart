0a67abffb78e037b58f1c7b219f3e1a68275640a
SSebigo <grezesfrancois@gmail.com>
2022-01-04 22:33:50 +0100

    add bookmarked + liked + settings

--- lib/application/series/series_state.dart
+++ lib/application/series/series_state.dart
@@ -7,17 +7,23 @@ class SeriesState with _$SeriesState {
   const factory SeriesState({
     required User author,
     required Option<Result<None, CoreFailure>> failureOption,
+    required bool isBookmarked,
+    required bool isLiked,
     required bool isProcessing,
     required Series series,
     required User session,
+    required Settings settings,
   }) = _SeriesState;
 
   /// @nodoc
   factory SeriesState.initial() => SeriesState(
         author: User.empty(),
         failureOption: const None(),
+        isBookmarked: false,
+        isLiked: false,
         isProcessing: false,
         series: Series.empty(),
         session: User.empty(),
+        settings: Settings.empty(),
       );
 }
