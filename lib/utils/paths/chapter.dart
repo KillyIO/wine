0213480bf0dec0ea83465c73145ef977bd7cb9c2
SSebigo <grezesfrancois@gmail.com>
2022-01-23 20:29:53 +0100

    add chapters related firebase paths

--- lib/utils/paths/chapter.dart
+++ lib/utils/paths/chapter.dart
@@ -1,2 +1,11 @@
 /// @nodoc
+const String chaptersBookmarksPath = 'chapters_bookmarks';
+
+/// @nodoc
+const String chaptersLikesPath = 'chapters_likes';
+
+/// @nodoc
 const String chaptersPath = 'chapters';
+
+/// @nodoc
+const String chaptersViewsPath = 'chapters_views';
