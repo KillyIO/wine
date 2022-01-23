fdafe6974cf65c5893d910aab39ba1f0402321c4
SSebigo <grezesfrancois@gmail.com>
2022-01-23 20:10:26 +0100

    remove unused import

--- lib/infrastructure/default_covers/default_covers_repository.dart
+++ lib/infrastructure/default_covers/default_covers_repository.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:injectable/injectable.dart';
 import 'package:isar/isar.dart';
