eca785f7d715cac2b2331598df137a4c56fb9a27
SSebigo <grezesfrancois@gmail.com>
2022-01-31 00:08:49 +0100

    move blocbuilder for more precise updates

--- lib/presentation/library/library_trees_layout.dart
+++ lib/presentation/library/library_trees_layout.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:wine/application/library/library_bloc.dart';
 import 'package:wine/domain/tree/tree.dart';
-import 'package:wine/presentation/library/library_base_tree_layout.dart';
+import 'package:wine/presentation/library/library_base_trees_layout.dart';
 import 'package:wine/presentation/library/widgets/library_vertical_navbar.dart';
 import 'package:wine/utils/constants/library.dart';
 
@@ -11,7 +11,7 @@ class LibraryTreesLayout extends StatelessWidget {
   /// @nodoc
   const LibraryTreesLayout({Key? key}) : super(key: key);
 
-  List<Tree> _getTree(LibraryState state, String type) {
+  List<Tree> _getTrees(LibraryState state, String type) {
     switch (type) {
       case 'published':
         return state.trees.where((s) => s.isPublished == true).toList();
@@ -28,31 +28,39 @@ class LibraryTreesLayout extends StatelessWidget {
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context).size;
 
-    return BlocBuilder<LibraryBloc, LibraryState>(
-      builder: (context, state) {
-        return Row(
-          children: <Widget>[
-            LibraryVerticalNavbar(
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        BlocBuilder<LibraryBloc, LibraryState>(
+          buildWhen: (previous, current) =>
+              current.currentVerticalNavbarIdx !=
+              previous.currentVerticalNavbarIdx,
+          builder: (context, state) {
+            return LibraryVerticalNavbar(
               currentIndex: state.currentVerticalNavbarIdx,
               items: libraryVerticalNavbarKeys,
               width: mediaQuery.width * .2,
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.only(right: 20),
-                child: LibraryBaseTreeLayout(
-                  treeList: _getTree(
+            );
+          },
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.only(right: 20),
+            child: BlocBuilder<LibraryBloc, LibraryState>(
+              builder: (context, state) {
+                return LibraryBaseTreesLayout(
+                  trees: _getTrees(
                     state,
                     libraryVerticalNavbarKeys[state.currentVerticalNavbarIdx],
                   ),
                   type:
                       libraryVerticalNavbarKeys[state.currentVerticalNavbarIdx],
-                ),
-              ),
+                );
+              },
             ),
-          ],
-        );
-      },
+          ),
+        ),
+      ],
     );
   }
 }
